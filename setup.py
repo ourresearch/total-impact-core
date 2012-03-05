a0045395d384e23a3ccb2aa5868594908bd89269
Mark MacGillivray <mark@cottagelabs.com>
2012-03-05 17:34:03 +0000

    adding basic repo layout

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'total-impact',
+    version = '0.2.0',
+    packages = find_packages(),
+    install_requires = [
+        "Flask==0.7.2",
+        "Flask-Login",
+        "Flask-WTF",
+				],
+    url = 'http://bibserver.okfn.org/',
+    author = 'Total Impact',
+    author_email = '',
+    description = 'Total Impact',
+    license = '',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
+
