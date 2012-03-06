bebabdc2925abe6263b2a180a4b9a2d335b9b2c4
Jason Priem <jason@jasonpriem.com>
2012-03-06 02:11:49 +0000

    aliases get_ids and add_aliases work

--- test/totalimpact/test_models.py
+++ test/totalimpact/test_models.py
@@ -6,7 +6,34 @@ def setup_module():
 def teardown_module():
     pass
 
-def testModelsInstantiates():
+def test1():
+    ''' alias gives self a tiid at creation'''
     a = models.aliases()
     assert len(a.tiid) == 36, "len was " + str(len(a.tiid))
+    
+def test2():
+    '''adds new aliases to the  object'''
+    a = models.aliases()
+    
+    a.add_alias("my_namespace", "my_id")
+    assert a.data == {"my_namespace": ["my_id"]}, a.data
+    
+    a.add_alias("my_namespace", "my_2nd_id")
+    assert a.data == {"my_namespace": ["my_id", "my_2nd_id"]}, a.data
+    
+    a.add_alias("my_2nd_namespace", "my_id")
+    assert a.data == {"my_namespace": ["my_id", "my_2nd_id"], \
+        "my_2nd_namespace":["my_id"]}, a.data
+        
+def test3():
+    '''gets an alias based on its namespace'''
+    a = models.aliases()
+    a.add_alias("my_namespace", "my_id")
+    a.add_alias("my_namespace", "my_2nd_id")
+    
+    res = a.get_ids("my_namespace")
+    assert res == ["my_id", "my_2nd_id"], res
+    
+    failres = a.get_ids("my_missing_namespace")
+    assert failres == [], failres
     
--- totalimpact/models.py
+++ totalimpact/models.py
@@ -1,16 +1,26 @@
 import uuid
+from collections import defaultdict
 
 class aliases:
     ''' handles all the identifiers for an Item.'''
     
     def __init__(self):
         self.tiid = str(uuid.uuid1())
+        self.data = defaultdict(list)
+    
+    def get_ids(self, namespace): 
+        ''' gets list of this object's ids in a given namespace
         
+        returns [] if no ids
+        '''
+        return self.data[namespace]
+    
+    def add_alias(self, namespace, id):
+        self.data[namespace].append(id)
+        
+    
+    
         
 
 class metrics:
     pass
-
-
-a = aliases()
-print a.tiid
