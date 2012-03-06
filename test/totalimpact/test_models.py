64e690ae17d23d30eac951d5846672bc678c4e12
Jason Priem <jason@jasonpriem.com>
2012-03-06 03:24:12 +0000

    get_aliases returns list of (namespace, id) tuples based on list of namespace args

--- test/totalimpact/test_models.py
+++ test/totalimpact/test_models.py
@@ -1,39 +1,43 @@
 from totalimpact import models
 
-def setup_module():
-    pass
+class test_aliases:
+    def setup(self):
+        self.a = models.aliases()
+        self.a.add_alias("foo", "id1")
+        self.a.add_alias("foo", "id2")
+        self.a.add_alias("bar", "id1") 
 
-def teardown_module():
-    pass
+    def teardown(self):
+        pass
 
-def test1():
-    ''' alias gives self a tiid at creation'''
-    a = models.aliases()
-    assert len(a.tiid) == 36, "len was " + str(len(a.tiid))
-    
-def test2():
-    '''adds new aliases to the  object'''
-    a = models.aliases()
-    
-    a.add_alias("my_namespace", "my_id")
-    assert a.data == {"my_namespace": ["my_id"]}, a.data
-    
-    a.add_alias("my_namespace", "my_2nd_id")
-    assert a.data == {"my_namespace": ["my_id", "my_2nd_id"]}, a.data
-    
-    a.add_alias("my_2nd_namespace", "my_id")
-    assert a.data == {"my_namespace": ["my_id", "my_2nd_id"], \
-        "my_2nd_namespace":["my_id"]}, a.data
+    def test1(self):
+        ''' alias gives self a tiid at creation'''
+
+        assert len(self.a.tiid) == 36, "len was " + str(len(self.a.tiid))
+
+    def test2(self):
+        '''adds new aliases to the  object'''
+
+        assert self.a.data == {"foo": ["id1", "id2"], \
+            "bar":["id1"]}, self.a.data
+
+    def test3(self):
+        '''gets an alias based on its namespace'''
+
+        res = self.a.get_ids_by_namespace("foo")
+        assert res == ["id1", "id2"], res
+
+        failres = self.a.get_ids_by_namespace("my_missing_namespace")
+        assert failres == [], failres
+
+    def test4(self):
+        '''gets a list of aliases from a list of namespaces'''
         
-def test3():
-    '''gets an alias based on its namespace'''
-    a = models.aliases()
-    a.add_alias("my_namespace", "my_id")
-    a.add_alias("my_namespace", "my_2nd_id")
-    
-    res = a.get_ids("my_namespace")
-    assert res == ["my_id", "my_2nd_id"], res
-    
-    failres = a.get_ids("my_missing_namespace")
-    assert failres == [], failres
+        res = self.a.get_aliases(["foo", "bar"])
+        expected = [("foo", "id1"), ("foo", "id2"), ("bar", "id1")]
+        assert res == expected, res
+
+        res = self.a.get_aliases(["foo"])
+        expected = [("foo", "id1"), ("foo", "id2")]
+        assert res == expected, res
     
--- totalimpact/models.py
+++ totalimpact/models.py
@@ -8,12 +8,26 @@ class aliases:
         self.tiid = str(uuid.uuid1())
         self.data = defaultdict(list)
     
-    def get_ids(self, namespace): 
-        ''' gets list of this object's ids in a given namespace
+    def get_aliases(self, namespace_list): 
+        ''' gets list of this object's aliases in each given namespace
+        
+        returns a list of (namespace, id) tuples
+        '''
+        
+        ret = []
+        for namespace in namespace_list:
+            ids = self.get_ids_by_namespace(namespace)
+            for id in ids:
+                ret.append((namespace, id))
+        
+        return ret
+    
+    def get_ids_by_namespace(self, namespace):
+        ''' gets list of this object's ids in each given namespace
         
         returns [] if no ids
         '''
-        return self.data[namespace]
+        return self.data[namespace]    
     
     def add_alias(self, namespace, id):
         self.data[namespace].append(id)
