9485d6d96edb16680552ca5f4445ec2d5715ea51
Mark MacGillivray <mark@cottagelabs.com>
2012-03-08 22:08:09 +0000

    fixing syntax error in models and removing old commented out stuff

--- totalimpact/models.py
+++ totalimpact/models.py
@@ -244,7 +244,7 @@ class ProviderMetric(object):
     {
         "id": "Mendeley:readers",
         "value": 16,
-        "last_update": 1328569492.406,
+        "last_modified": 1328569492.406,
         "provenance_url": "http:\/\/api.mendeley.com\/research\/public-chemical-compound-databases\/",
         "meta": {
             "display_name": "readers"
@@ -347,7 +347,8 @@ class Aliases(object):
         "tiid":"123456",
         "title":["Why Most Published Research Findings Are False"],
         "url":["http:\/\/www.plosmedicine.org\/article\/info:doi\/10.1371\/journal.pmed.0020124"],
-        "doi": ["10.1371\/journal.pmed.0020124"]
+        "doi": ["10.1371\/journal.pmed.0020124"],
+        "last_modified": 1328569492.406
         ...
     }
     """
@@ -430,156 +431,3 @@ class Aliases(object):
     def __str__(self):
         return "TIID: " + self.tiid + " " + str(self.data)
 
-"""
-import uuid, os, json
-import totalimpact.dao as dao
-from collections import defaultdict
-
-from werkzeug import generate_password_hash, check_password_hash
-from flaskext.login import UserMixin
-
-class emptyMetricsError(Exception):
-    pass
-
-class Aliases:
-    ''' handles all the identifiers for an Item.'''
-    
-    def __init__(self, seed=None):
-        if seed is None:
-            self.tiid = str(uuid.uuid1())
-            self.data = defaultdict(list)
-        else:
-            self._validate_seed(seed)
-            self.tiid = seed.get("tiid", str(uuid.uuid1()))
-            self.data = defaultdict(list, seed)
-    
-    def _validate_seed(self, seed):
-        # FIXME: what does this actually do?
-        pass
-    
-    def get_aliases_list(self, namespace_list): 
-        ''' gets list of this object's aliases in each given namespace
-        
-        returns a list of (namespace, id) tuples
-        '''
-        
-        ret = []
-        for namespace in namespace_list:
-            ids = self.get_ids_by_namespace(namespace)
-            for id in ids:
-                ret.append((namespace, id))
-        
-        return ret
-    
-    def get_aliases_dict(self):
-        return self.data
-        
-    def get_ids_by_namespace(self, namespace):
-        ''' gets list of this object's ids in each given namespace
-        
-        returns [] if no ids
-        >>> a = Aliases()
-        >>> a.add_alias("foo", "id1")
-        >>> a.get_ids_by_namespace("foo")
-        ['id1']
-        '''
-        return self.data[namespace]    
-    
-    def add_alias(self, namespace, id):
-        self.data[namespace].append(id) # using defaultdict, no need to test if list exists first
-        
-    def add_unique(self, alias_list):
-        for ns, id in alias_list:
-            if id not in self.data[ns]:
-                self.add_alias(ns, id)
-    
-    def __repr__(self):
-        return "TIID: " + self.tiid + " " + str(self.data)
-        
-    def __str__(self):
-        return "TIID: " + self.tiid + " " + str(self.data)
-
-class Metrics:
-
-    def __init__(self, seed=None):
-        self.properties = {} if seed is None else seed
-        #here = os.path.dirname(os.path.realpath(__file__))
-        #fp = open(here + '/../test/complete_metric.json')
-        #self.example_seed = json.load(fp)
-    
-    def add(self, property, value):
-        self.properties[property] = value
-        
-    def get(self, property, default_value):
-        return self.properties.get(property, default_value)
-        
-    def add_metrics(self, other):
-        # FIXME: use dstruct, or some other dictionary stitching algorithm
-        # this just replaces everything in the current object with the other
-        # object - nothing additive about it
-        for p in other.properties.keys():
-            self.properties[p] = other.properties[p]
-            
-    def __repr__(self):
-        return str(self.properties)
-        
-    def __str__(self):
-        return str(self.properties)
-
-    def is_complete(self):
-        if 0:
-            return True
-        else:
-            return False
- 
-class Item(dao.Dao):
-    __type__ = 'item'
-    
-    @property
-    def aliases(self):
-        try:
-            return self._als
-        except:
-            self._als = Aliases(seed=self.data.get('aliases',{}))
-            return self._als
-
-    @property
-    def metrics(self):
-        try:
-            return self._ms
-        except:
-            self._ms = Metrics(seed=self.data.get('metrics',{}))
-            return self._ms
-        
-    @property
-    def data(self):
-        try:
-            self._data['aliases'] = self._als.get_aliases_dict
-        except:
-            pass
-        try:
-            self._data['metrics'] = dict(self._ms)
-        except:
-            pass
-        return self._data
-    
-    # FIXME: Mark - can you check this is The Way with a Couch object?
-    def set_metrics(self, metrics):
-        self._ms = metrics
-        
-    def set_aliases(self, aliases):
-        self._als = aliases
-        
-class Collection(dao.Dao):
-    __type__ = 'collection'
-    
-class User(dao.Dao,UserMixin):
-    __type__ = 'user'
-
-    def set_password(self, password):
-        self.data['password'] = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.data['password'], password)
-
-"""
