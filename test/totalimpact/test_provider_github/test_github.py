e598968452ee4b8a35170de9b37f43d2992f9172
Heather Piwowar <hpiwowar@gmail.com>
2012-03-18 19:17:18 -0700

    add github and pubmed member_item methods

---
+++ test/totalimpact/test_provider_github/test_github.py
@@ -0,0 +1,99 @@
+from totalimpact.models import Metrics, Aliases
+from totalimpact.config import Configuration
+from totalimpact.providers.github import Github
+from totalimpact.providers.provider import Provider, ProviderClientError, ProviderServerError
+
+import os, unittest
+
+# prepare a monkey patch to override the http_get method of the Provider
+class DummyResponse(object):
+    def __init__(self, status, content):
+        self.status_code = status
+        self.text = content
+
+def get_memberitems_user_html(self, url, headers=None, timeout=None):
+    f = open(GITHUB_MEMBERITEMS_USER_HTML, "r")
+    return DummyResponse(200, f.read())
+
+def get_memberitems_orgs_html(self, url, headers=None, timeout=None):
+    f = open(GITHUB_MEMBERITEMS_ORGS_HTML, "r")
+    return DummyResponse(200, f.read())
+
+# dummy Item class
+class Item(object):
+    def __init__(self, aliases=None):
+        self.aliases = aliases
+
+CWD, _ = os.path.split(__file__)
+
+APP_CONFIG = os.path.join(CWD, "..", "test.conf.json")
+GITHUB_MEMBERITEMS_USER_HTML = os.path.join(CWD, "sample_extract_user_metrics.json")
+GITHUB_MEMBERITEMS_ORGS_HTML = os.path.join(CWD, "sample_extract_orgs_metrics.json")
+DOI = "10.5061/dryad.7898"
+
+class Test_Github(unittest.TestCase):
+
+    def setUp(self):
+        print APP_CONFIG
+        self.config = Configuration(APP_CONFIG, False)
+    
+    def tearDown(self):
+        pass
+    
+    def test_01_init(self):
+        # first ensure that the configuration is valid
+        assert len(self.config.cfg) > 0
+        
+        # can we get the config file
+        dcfg = None
+        for p in self.config.providers:
+            if p["class"].endswith("github.Github"):
+                dcfg = p["config"]
+        print dcfg
+        assert os.path.isfile(dcfg)
+        
+        # instantiate the configuration
+        dconf = Configuration(dcfg, False)
+        assert len(dconf.cfg) > 0
+        
+        # basic init of provider
+        provider = Github(dconf, self.config)
+        assert provider.config is not None
+        
+        ## FIXME implement state
+        #assert provider.state is not None
+
+        assert provider.id == dconf.id
+        
+    def test_02_implements_interface(self):
+        # ensure that the implementation has all the relevant provider methods
+        dcfg = None
+        for p in self.config.providers:
+            if p["class"].endswith("github.Github"):
+                dcfg = p["config"]
+        dconf = Configuration(dcfg, False)
+        provider = Github(dconf, self.config)
+        
+        # must have the four core methods
+        assert hasattr(provider, "member_items")
+        assert hasattr(provider, "aliases")
+        assert hasattr(provider, "metrics")
+        assert hasattr(provider, "provides_metrics")
+    
+
+    def test_04_member_items(self):        
+        dcfg = None
+        for p in self.config.providers:
+            if p["class"].endswith("github.Github"):
+                dcfg = p["config"]
+        dconf = Configuration(dcfg, False)
+        provider = Github(dconf, self.config)
+        
+
+        Provider.http_get = get_memberitems_user_html
+        members = provider.member_items("egonw", "githubUser")
+        assert len(members) >= 30, (len(members), members)
+
+        Provider.http_get = get_memberitems_orgs_html
+        members = provider.member_items("bioperl", "githubOrg")
+        assert len(members) >= 32, (len(members), members)
---
+++ totalimpact/providers/github.py
@@ -0,0 +1,43 @@
+import time, re, urllib
+from provider import Provider, ProviderError, ProviderTimeout, ProviderServerError, ProviderClientError, ProviderHttpError, ProviderState
+from totalimpact.models import Metrics, ProviderMetric
+from BeautifulSoup import BeautifulStoneSoup
+import requests
+import simplejson
+
+from totalimpact.tilogging import logging
+logger = logging.getLogger(__name__)
+
+class GithubMetricSnapshot(ProviderMetric):
+    def __init__(self, provider, id, value):
+        meta = provider.config.metrics["static_meta"][id]
+        super(GithubMetricSnapshot, self).__init__(id=id, value=value, meta=meta)
+
+class Github(Provider):  
+
+    def __init__(self, config, app_config):
+        super(Github, self).__init__(config, app_config)
+        self.id = self.config.id
+        self.member_items_rx = re.compile(r"<Id>(.*)</Id>")
+        
+    def member_items(self, query_string, query_type):
+        enc = urllib.quote(query_string)
+
+        url = self.config.member_items["querytype"][query_type]['url'] % enc
+        logger.debug(self.config.id + ": query type " + query_type)
+        logger.debug(self.config.id + ": attempting to retrieve member items from " + url)
+        
+        # try to get a response from the data provider        
+        response = self.http_get(url, timeout=self.config.member_items.get('timeout', None))
+        
+        #hits = self.github_member_items_rx.findall(response.text)
+        hits = simplejson.loads(response.text)
+        hits = [hit["name"] for hit in hits]
+
+        return [("Github", (query_string, hit)) for hit in list(set(hits))]
+    
+
+        
+  
+
+
