c47827056a50d0875126466aa853553a97ee4eba
Heather Piwowar <hpiwowar@gmail.com>
2012-03-23 21:51:42 -0700

    refactor provider setup into setUp fixture

--- test/totalimpact/test_provider_dryad/test_dryad.py
+++ test/totalimpact/test_provider_dryad/test_dryad.py
@@ -6,28 +6,34 @@ from totalimpact.providers.provider import Provider, ProviderClientError, Provid
 import os, unittest
 from nose.tools import nottest
 
+# dummy Item class
+class Item(object):
+    def __init__(self, aliases=None):
+        self.aliases = aliases
+
 # prepare a monkey patch to override the http_get method of the Provider
 class DummyResponse(object):
     def __init__(self, status, content):
         self.status_code = status
         self.text = content
-def get_member_items_html(self, url, headers=None, timeout=None):
+
+def get_member_items_html_success(self, url, headers=None, timeout=None):
     f = open(SAMPLE_EXTRACT_MEMBER_ITEMS_PAGE, "r")
     return DummyResponse(200, f.read())
-def successful_get(self, url, headers=None, timeout=None):
+
+def get_aliases_html_success(self, url, headers=None, timeout=None):
     f = open(SAMPLE_EXTRACT_ALIASES_PAGE, "r")
     return DummyResponse(200, f.read())
+
 def get_empty(self, url, headers=None, timeout=None):
     return DummyResponse(200, "")
+
 def get_400(self, url, headers=None, timeout=None):
     return DummyResponse(400, "")
+
 def get_500(self, url, headers=None, timeout=None):
     return DummyResponse(500, "")
 
-# dummy Item class
-class Item(object):
-    def __init__(self, aliases=None):
-        self.aliases = aliases
 
 CWD, _ = os.path.split(__file__)
 
@@ -44,92 +50,63 @@ class Test_Dryad(unittest.TestCase):
         print APP_CONFIG
         self.config = Configuration(APP_CONFIG, False)
         self.old_http_get = Provider.http_get
+
+        dryad_config_filename = None
+        for p in self.config.providers:
+            if p["class"].endswith("dryad.Dryad"):
+                dryad_config_filename = p["config"]
+
+        dryad_config = Configuration(dryad_config_filename, False)
+        self.provider = Dryad(dryad_config, self.config)
+
     
     def tearDown(self):
         Provider.http_get = self.old_http_get
     
+
     def test_01_init(self):
-        # first ensure that the configuration is valid
+        # ensure that the configuration is valid
         assert len(self.config.cfg) > 0
+        assert self.provider.config is not None
+        assert self.provider.state is not None
+        assert self.provider.id == "Dryad"
         
-        # can we get the dryad config file
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        print dcfg
-        assert os.path.isfile(dcfg)
-        
-        # instantiate the configuration
-        dconf = Configuration(dcfg, False)
-        assert len(dconf.cfg) > 0
-        
-        # basic init of provider
-        provider = Dryad(dconf, self.config)
-        assert provider.config is not None
-        assert provider.state is not None
-        assert provider.id == dconf.id
-        
+
     def test_02_implements_interface(self):
         # ensure that the implementation has all the relevant provider methods
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
         
         # must have the four core methods
-        assert hasattr(provider, "member_items")
-        assert hasattr(provider, "aliases")
-        assert hasattr(provider, "metrics")
-        assert hasattr(provider, "provides_metrics")
+        assert hasattr(self.provider, "member_items")
+        assert hasattr(self.provider, "aliases")
+        assert hasattr(self.provider, "metrics")
+        assert hasattr(self.provider, "provides_metrics")
     
+
     def test_03_crossref_doi(self):
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
-        
         # check that the regex is set
-        assert provider.crossref_rx is not None
+        assert self.provider.crossref_rx is not None
         
         # ensure that it matches an appropriate DOI
-        assert provider.crossref_rx.search(DOI) is not None
+        assert self.provider.crossref_rx.search(DOI) is not None
         
         # ensure that it doesn't match an inappropriate DOI
-        assert provider.crossref_rx.search("11.12354/bib") is None
+        assert self.provider.crossref_rx.search("11.12354/bib") is None
         
         # now make sure that the built in method gets the same results
-        assert provider._is_crossref_doi(("DOI", DOI))
-        assert not provider._is_crossref_doi(("DOI", "11.12354/bib"))
+        assert self.provider._is_crossref_doi(("DOI", DOI))
+        assert not self.provider._is_crossref_doi(("DOI", "11.12354/bib"))
     
+
     def test_04_member_items(self):
-        Provider.http_get = get_member_items_html
+        Provider.http_get = get_member_items_html_success
         
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
-        
-        members = provider.member_items("Piwowar, Heather A.", "dryadAuthor")
+        members = self.provider.member_items("Piwowar, Heather A.", "dryadAuthor")
         assert len(members) == 4, str(members)
         
     def test_05_aliases_read_content(self):
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
-        
         # ensure that the dryad reader can interpret an xml doc appropriately
         f = open(SAMPLE_EXTRACT_ALIASES_PAGE, "r")
-        aliases = provider._extract_aliases(f.read())
+        aliases = self.provider._extract_aliases(f.read())
         assert len(aliases) == 2, aliases
         
         assert ("URL", u'http://hdl.handle.net/10255/dryad.7898') in aliases
@@ -137,38 +114,17 @@ class Test_Dryad(unittest.TestCase):
     
     def test_06_aliases_400(self):
         Provider.http_get = get_400
-        
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
-        
-        self.assertRaises(ProviderClientError, provider._get_aliases, DOI)
+        self.assertRaises(ProviderClientError, self.provider._get_aliases, DOI)
         
     def test_07_aliases_500(self):
         Provider.http_get = get_500
         
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
-        
-        self.assertRaises(ProviderServerError, provider._get_aliases, DOI)
+        self.assertRaises(ProviderServerError, self.provider._get_aliases, DOI)
     
     def test_08_aliases_success(self):
-        Provider.http_get = successful_get
+        Provider.http_get = get_aliases_html_success
         
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
-        
+
         # FIXME add proper tests for aliases
         #aliases = provider._get_aliases(DOI)
         #assert len(aliases) == 1, aliases
@@ -180,13 +136,6 @@ class Test_Dryad(unittest.TestCase):
     def test_09_aliases_empty_success(self):
         Provider.http_get = get_empty
         
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
-        
         # FIXME add proper tests for aliases        
         #aliases = provider._get_aliases(DOI)
         #assert len(aliases) == 1
@@ -194,30 +143,16 @@ class Test_Dryad(unittest.TestCase):
     def test_10_aliases_general_fail(self):
         Provider.http_get = get_400
         
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
-        
         d = {"DOI" : ["10.1371/journal.pcbi.1000361"], "URL" : ["http://cottagelabs.com"]}
         alias = Aliases(seed=d)
         item = Item(aliases=alias)
-        item = provider.aliases(item)
+        item = self.provider.aliases(item)
         
         # the aliases should be unchanged
         assert item.aliases == alias
     
     def test_11_aliases_general_success(self):
-        Provider.http_get = successful_get
-        
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
+        Provider.http_get = get_aliases_html_success
         
         d = {"DOI" : [DOI], "URL" : ["http://cottagelabs.com"]}
         alias = Aliases(seed=d)
@@ -234,49 +169,25 @@ class Test_Dryad(unittest.TestCase):
 
     @nottest
     def test_12_provides_metrics(self):
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
 
-        assert provider.provides_metrics() == True
+        assert self.provider.provides_metrics() == True
 
     def test_13_show_details_url(self):
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
 
-        assert provider.get_show_details_url(DOI) == "http://dx.doi.org/" + DOI
+        assert self.provider.get_show_details_url(DOI) == "http://dx.doi.org/" + DOI
     
 
     def test_14_basic_extract_stats(self):
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
         
         f = open(SAMPLE_EXTRACT_METRICS_PAGE, "r")
-        ret = provider._extract_stats(f.read())
+        ret = self.provider._extract_stats(f.read())
         assert len(ret) == 4, ret
 
     @nottest
     ## FIXME supposed to take an alias metric
     def test_15_metrics(self):
-        dcfg = None
-        for p in self.config.providers:
-            if p["class"].endswith("dryad.Dryad"):
-                dcfg = p["config"]
-        dconf = Configuration(dcfg, False)
-        provider = Dryad(dconf, self.config)
         
-        ret = provider.metrics(DOI)
+        ret = self.provider.metrics(DOI)
         assert len(ret.str_list_provider_metrics()) == 4, len(ret.str_list_provider_metrics())
 
     
