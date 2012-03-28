454f0e8ab116c70f65b8a3855693a1cf0d7990ee
Jason Priem <jp@jasonpriem.org>
2012-03-28 16:41:52 -0400

    added docs and more type checking to views

--- config/couch/views/aliases.js
+++ config/couch/views/aliases.js
@@ -1,3 +1,8 @@
 function(doc) {
-    emit([doc.aliases.last_modified],doc);
+    // lists items, ordered by the last-modified time of their aliases.
+    if (typeof doc.aliases != "undefined") {
+        if (typeof doc.aliases.last_modified != "undefined") {
+            emit([doc.aliases.last_modified],doc);
+        }
+    }
 }
