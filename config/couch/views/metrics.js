4c5a31592318f8f11ada413446bf496249898f35
Jason Priem <jp@jasonpriem.org>
2012-03-28 16:41:52 -0400

    added docs and more type checking to views

--- config/couch/views/metrics.js
+++ config/couch/views/metrics.js
@@ -1,9 +1,16 @@
 function(doc) {
-    if ( doc.metrics != "undefined") {
-        for (var metric in doc.metrics.meta) {
-            var item = doc.metrics.meta[metric];
-            if ( !item.ignore ) {
-                emit([metric,item.last_requested,item.last_modified], doc);
+    // for items that have aliases, lists items sorted by provider, last request
+    //    time, and last update time
+    //
+    // I (jason) am not sure if we metrics.meta to be keyed by provider or
+    // metric name? I think the latter (as it is now), but the former makes
+    // things easier for the providers consuming the queue I think.
+
+    if (typeof doc.metrics != "undefined" && typeof doc.metrics.meta != "undefined") {
+        for (var metricName in doc.metrics.meta) {
+            var metricData = doc.metrics.meta[metricName];
+            if ( !metricData.ignore ) {
+                emit([metric, metricData.last_requested, metricData.last_modified], doc);
             }
         }
     }
