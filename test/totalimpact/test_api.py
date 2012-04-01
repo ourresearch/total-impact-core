9b14a5f7f1a42562736dcb5df3052387a37aa709
Heather Piwowar <hpiwowar@gmail.com>
2012-03-31 20:11:41 -0700

    add api unit test for member_items

--- test/totalimpact/test_api.py
+++ test/totalimpact/test_api.py
@@ -1,16 +1,37 @@
 import unittest, json
-from nose.tools import nottest
+from nose.tools import nottest, assert_equals
 
 from totalimpact import api
+from totalimpact.providers.dryad import Dryad
+
+
+TEST_DRYAD_DOI = "10.5061/dryad.7898"
+GOLD_MEMBER_ITEM_CONTENT = ["MEMBERITEM CONTENT"]
+
+def MOCK_member_items(self, a, b):
+    return(GOLD_MEMBER_ITEM_CONTENT)
+
 
 class TestWeb(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.app = api.app.test_client()
 
+        # Mock out relevant methods of the Dryad provider
+        self.orig_Dryad_member_items = Dryad.member_items
+        Dryad.member_items = MOCK_member_items
 
     def tearDown(self):
-        pass
+        Dryad.member_items = self.orig_Dryad_member_items
+
+    def test_memberitems_get(self):
+        response = self.app.get('/provider/Dryad/memberitems?query=Otto%2C%20Sarah%20P.&type=author')
+        print response
+        print response.data
+        assert_equals(response.status_code, 200)
+        assert_equals(json.loads(response.data), GOLD_MEMBER_ITEM_CONTENT)
+        assert_equals(response.mimetype, "application/json")
+
 
     # FIXME: this test is breaking
     @nottest
