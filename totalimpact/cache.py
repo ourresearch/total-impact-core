2663e6021197f1f5d5fbb5019997ca5abb7f12a8
Kevin Campbell <kev@larkspur.weegie.net>
2012-04-10 18:39:57 +0100

    Adding in a http cache for the providers. This uses memcached and url as key
    Includes a test. Apologies if I make some mistakes, first commit :)

--- totalimpact/cache.py
+++ totalimpact/cache.py
@@ -1,53 +1,26 @@
-class Cache(object):
-    def http_get(self, url):
-        return None
-        
-    def cache_http_get(self, url, content):
-        pass
-        
-    def get_cache_timeout_response(self,
-                                    url,
-                                    http_timeout_in_seconds = 20,
-                                    max_cache_age_seconds = (1) * (24 * 60 * 60), # (number of days) * (number of seconds in a day),
-                                    header_addons = {}):
+import hashlib
+import memcache
 
+class CacheException(Exception):
+    pass
+
+class Cache(object):
+    """ Maintains a cache of URL responses in memcached """
+
+    def __init__(self, max_cache_age=86400):
+        self.max_cache_age = max_cache_age
+
+    def get_cache_entry(self, url):
+        """ Get an entry from the cache, returns None if not found """
         key = hashlib.md5(url).hexdigest()
         mc = memcache.Client(['127.0.0.1:11211'])
-        response = mc.get(key)
-        if response:
-            self.status["count_got_response_from_cache"] += 1
-        else:
-            http = httplib2.Http(timeout=http_timeout_in_seconds)
-            response = http.request(url)
-            mc.set(key, response, max_cache_age_seconds)
-            self.status["count_api_requests"] += 1
+        return mc.get(key)
 
-        # This is the old, file-based caching system.
-        # I left some stuff out; Heather, feel free to move up.
-        '''
-        cache_read = http_cached.cache.get(url)
-                self.status["count_got_response_from_cache"] += 1
-        if (cache_read):
-            (response, content) = cache_read.split("\r\n\r\n", 1)
-        else:
-            ## response['cache-control'] = "max-age=" + str(max_cache_age_seconds)
-            ## httplib2._updateCache(header_dict, response, content, http_cached.cache, url)
-            if response.fromcache:
-            else:
-                self.status["count_missed_cache"] += 1
-                self.status["count_cache_miss_details"] = str(self.status["count_cache_miss_details"]) + "; " + url
-                self.status["count_cache_miss_response"] = str(response)
-                self.status["count_api_requests"] += 1
-
-            if False:
-                self.status["count_request_exception"] = "EXCEPTION!"
-                self.status["count_uncached_call"] += 1
-                self.status["count_api_requests"] += 1
-                #(response, content) = http_cached.request(url, headers=header_dict.update({'cache-control':'no-cache'}))
-                req = urllib2.Request(url, headers=header_dict)
-                uh = urllib2.urlopen(req)
-                content = uh.read()
-                response = uh.info()
-        '''
-
-        return(response)
+    def set_cache_entry(self, url, data):
+        """ Store a cache entry """
+        key = hashlib.md5(url).hexdigest()
+        mc = memcache.Client(['127.0.0.1:11211'])
+        if not mc.set(key,data,time=self.max_cache_age):
+            raise CacheException("Unable to store into Memcached. Check config.")
+        
+        
