643a772952959d01f80e37e1adfd54686db90891
Kevin Campbell <kev@projectcolo.org.uk>
2012-04-30 22:55:33 +0100

    Functional tests now do very crude load testing

--- extras/functional_test.py
+++ extras/functional_test.py
@@ -8,6 +8,8 @@ import urllib2
 import urllib
 import json
 import time
+import sys
+import pickle
 from pprint import pprint
 
 class TotalImpactAPI:
@@ -30,78 +32,116 @@ class TotalImpactAPI:
 
     def request_item_result(self, item_id):
         url = self.base_url + urllib.quote('item/%s' % (item_id))
-        print url
         req = urllib2.Request(url)
         response = urllib2.urlopen(req)
         return json.loads(response.read())
         
 
+from optparse import OptionParser
 
-ti = TotalImpactAPI()
 
-# Request the items to be generated
-dryad_item = ti.request_item('doi','10.5061/dryad.7898')
-wikipedia_item = ti.request_item('doi', '10.1371/journal.pcbi.1000361')
-github_item = ti.request_item('github', 'egonw/gtd')
-
-time.sleep(10)
-
-# Try and get the result
-dryad_data = ti.request_item_result(dryad_item)
-wikipedia_data = ti.request_item_result(wikipedia_item)
-github_data = ti.request_item_result(github_item)
-
-checks = {
-    'wikipedia' : { 
-        'item': wikipedia_item,
-        'data': wikipedia_data,
-        'aliases': ['doi'],
-        'metrics' : {
-            'wikipedia:mentions' : 1
-        }
-    },
-    'github' : { 
-        'item': github_item,
-        'data': github_data,
-        'aliases': ['github'],
-        'metrics' : {
-            'github:forks' : 0,
-            'github:watchers' : 7
-        }
-    },
-    'dryad' : { 
-        'item': dryad_item,
-        'data': dryad_data,
-        'aliases': ['doi'],
-        'metrics' : {
-            'dryad:most_downloaded_file' : 63,
-            'dryad:package_views' : 149,
-            'dryad:total_downloads' : 169
+def checkItem(item, data, item_type, debug=False):
+    checks = {
+        'wikipedia' : { 
+            'aliases': ['doi'],
+            'metrics' : {
+                'wikipedia:mentions' : 1
+            }
+        },
+        'github' : { 
+            'aliases': ['github'],
+            'metrics' : {
+                'github:forks' : 0,
+                'github:watchers' : 7
+            }
+        },
+        'dryad' : { 
+            'aliases': ['doi','url','title'],
+            'metrics' : {
+                'dryad:most_downloaded_file' : 63,
+                'dryad:package_views' : 149,
+                'dryad:total_downloads' : 169
+            }
         }
     }
-}
 
+    aliases = checks[item_type]['aliases']
+    metrics = checks[item_type]['metrics']
 
-for provider in checks.keys():
-    item = checks[provider]['item']
-    data = checks[provider]['data']
-    aliases = checks[provider]['aliases']
-    metrics = checks[provider]['metrics']
-
-    print "Checking %s result (%s)..." % (provider, item)
+    if debug: print "Checking %s result (%s)..." % (item_type, item)
     
     # Check aliases are correct
     alias_result = set(data['aliases'].keys())
     if alias_result != set(['created','last_modified','last_completed'] + aliases):
-        print "Aliases is not correct, have %s" % alias_result
+        if debug: print "Aliases is not correct, have %s" % alias_result
+        return False
 
     # Check we've got some metric values
     for metric in metrics.keys():
         metric_data = data['metrics'][metric]['values']
         if len(metric_data) != 1:
-            print "Incorrect number of metric results for %s - %i" % (metric, len(metric_data))
+            if debug: print "Incorrect number of metric results for %s - %i" % (metric, len(metric_data))
+            return False
         else:
             if metric_data.values()[0] != metrics[metric]:
-                print "Incorrect metric result for %s - %s" % (metric, metric_data.values()[0])
+                if debug: print "Incorrect metric result for %s - %s" % (metric, metric_data.values()[0])
+                return False
 
+    return True
+
+
+
+
+if __name__ == '__main__':
+
+    parser = OptionParser()
+    parser.add_option("-s", "--simultaneous", dest="simultaneous", default=1,
+                      help="Number of simultaneous requests to make")
+    parser.add_option("-m", "--missing", dest="missing", default=False, action="store_true",
+                      help="Display any outstanding items")
+    (options, args) = parser.parse_args()
+
+
+    item_count = int(options.simultaneous)
+    
+    ti = TotalImpactAPI()
+
+    dryad_item = wikipedia_item = github_item = []
+    dryad_data = wikipedia_data = github_data = []
+    complete = {}
+    itemid = {}
+    for item_type in ['dryad','wikipedia','github']:
+        complete[item_type] = {}
+        itemid[item_type] = {}
+
+    for idx in range(item_count):
+        # Request the items to be generated
+        itemid['dryad'][idx] = ti.request_item('doi','10.5061/dryad.7898')
+        itemid['wikipedia'][idx] = ti.request_item('doi', '10.1371/journal.pcbi.1000361')
+        itemid['github'][idx] = ti.request_item('github', 'egonw/gtd')
+
+    for idx in range(item_count):
+        complete['dryad'][idx] = False
+        complete['wikipedia'][idx] = False
+        complete['github'][idx] = False
+
+    while True:
+
+        for idx in range(item_count):
+            for item_type in ['dryad','wikipedia','github']:
+                if not complete[item_type][idx]:
+                    if options.missing:
+                        print item_type, idx, itemid[item_type][idx]
+                    complete[item_type][idx] = checkItem(
+                        itemid[item_type][idx],
+                        ti.request_item_result(itemid[item_type][idx]),
+                        item_type, debug=options.missing
+                    )
+
+        total = sum([sum(complete[item_type].values()) for item_type in ['dryad','wikipedia','github']])
+        print [(item_type, sum(complete[item_type].values())) for item_type in ['dryad','wikipedia','github']], total
+        if total == item_count * 3:
+            sys.exit(0)    
+
+        time.sleep(1)
 
