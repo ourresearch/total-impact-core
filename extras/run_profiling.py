18fe34c77bbd2e60d8c214f441e027ff85278384
Heather Piwowar <hpiwowar@gmail.com>
2012-05-25 11:11:20 -0700

    basic profiling script for reference

---
+++ extras/run_profiling.py
@@ -0,0 +1,21 @@
+import yappi
+
+import os
+rootdir = "."
+logfile = os.path.join(rootdir, 'logs', 'total-impact.log')
+
+from totalimpact import backend
+
+yappi.clear_stats()
+yappi.start()
+backend.main(logfile)
+
+### Now, in another window run
+# ./services/api start
+# ./services/proxy start
+# ./extras/functional_test.py -i 6 -n 6
+# then when it is done, in python do a Cntl C to stop the backend and return to python prompt
+
+yappi.stop()
+
+yappi.print_stats(sort_type=yappi.SORTTYPE_TTOT, limit=30, thread_stats_on=False)
