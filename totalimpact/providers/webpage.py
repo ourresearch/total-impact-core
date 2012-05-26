a86ba71d6b0dddfd79c36df7a2e555d607c24400
Heather Piwowar <hpiwowar@gmail.com>
2012-05-25 19:32:13 -0700

    try using lxml.html to parse html for webpage bible

--- totalimpact/providers/webpage.py
+++ totalimpact/providers/webpage.py
@@ -1,5 +1,6 @@
 from totalimpact.providers import provider
 from totalimpact.providers.provider import Provider, ProviderContentMalformedError
+import lxml.html
 
 import logging
 logger = logging.getLogger('providers.webpage')
@@ -20,10 +21,16 @@ class Webpage(Provider):
         return("url" == namespace)
 
 
+    # use lxml because is html
     def _extract_biblio(self, page, id=None):
-        dict_of_keylists = {
-            'title' : ['html', 'head', 'title'],
-            'h1' : ['h1']
-        }
-        biblio_dict = provider._extract_from_xml(page, dict_of_keylists)
+        #dict_of_keylists = {
+        #    'title' : ['html', 'head', 'title'],
+        #    'h1' : ['h1']
+        #}
+
+        parsed_html = lxml.html.document_fromstring(page)
+        title = parsed_html.find(".//title").text
+        h1 = parsed_html.find(".//h1").text
+
+        biblio_dict = {'title':title, 'h1':h1}
         return biblio_dict    
