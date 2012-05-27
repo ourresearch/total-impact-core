18def508d28a09ef398ab15ef22a20d8d24c1533
Heather Piwowar <hpiwowar@gmail.com>
2012-05-27 06:52:34 -0700

    remove created from view key, breaks it for now

--- config/couch/views/by_tiid_with_snaps.js
+++ config/couch/views/by_tiid_with_snaps.js
@@ -1,7 +1,7 @@
 function(doc) {
   if (typeof doc.aliases != "undefined") {
-    emit([doc._id, 0, doc.created], doc);
+    emit([doc._id, 0], doc);
   } else if (typeof doc.drilldown_url != "undefined") {
-    emit([doc.tiid, 1, doc.created], doc);
+    emit([doc.tiid, 1], doc);
   }
 }
