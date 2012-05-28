d8e35fa4a05a9ab74a1abaa96fba7959e36ea8d0
Heather Piwowar <hpiwowar@gmail.com>
2012-05-28 07:04:13 -0700

    add slideshare provider

---
+++ test/unit_tests/providers/test_slideshare.py
@@ -0,0 +1,60 @@
+from test.unit_tests.providers import common
+from test.unit_tests.providers.common import ProviderTestCase
+from totalimpact.providers.provider import Provider, ProviderContentMalformedError
+from totalimpact.api import app
+
+import os
+import collections
+from nose.tools import assert_equals, raises, nottest, assert_true
+
+datadir = os.path.join(os.path.split(__file__)[0], "../../../extras/sample_provider_pages/slideshare")
+SAMPLE_EXTRACT_MEMBER_ITEMS_PAGE = os.path.join(datadir, "members")
+SAMPLE_EXTRACT_METRICS_PAGE = os.path.join(datadir, "metrics")
+SAMPLE_EXTRACT_ALIASES_PAGE = os.path.join(datadir, "aliases")
+SAMPLE_EXTRACT_BIBLIO_PAGE = os.path.join(datadir, "biblio")
+
+TEST_URL = "http://www.slideshare.net/cavlec/manufacturing-serendipity-12176916"
+TEST_SLIDESHARE_USER = "cavlec"
+
+class TestSlideshare(ProviderTestCase):
+
+    provider_name = "slideshare"
+
+    testitem_members = TEST_URL
+    testitem_aliases = ("url", TEST_URL)
+    testitem_metrics = ("url", TEST_URL)
+    testitem_biblio = ("url", TEST_URL)
+
+    def setUp(self):
+        ProviderTestCase.setUp(self)
+
+    def test_is_relevant_alias(self):
+        # ensure that it matches an appropriate ids
+        assert_equals(self.provider.is_relevant_alias(self.testitem_aliases), True)
+
+        assert_equals(self.provider.is_relevant_alias(("github", "egonw,cdk")), False)
+  
+    def test_extract_members(self):
+        f = open(SAMPLE_EXTRACT_MEMBER_ITEMS_PAGE, "r")
+        members = self.provider._extract_members(f.read(), TEST_SLIDESHARE_USER)
+        assert_equals(len(members), 36)
+        assert_true('url', u'http://www.slideshare.net/cavlec/avoiding-heronway' in members)
+
+    def test_extract_biblio(self):
+        f = open(SAMPLE_EXTRACT_BIBLIO_PAGE, "r")
+        ret = self.provider._extract_biblio(f.read())
+        assert_equals(ret, {'username': u'cavlec', 'title': u'Manufacturing Serendipity', 'repository': 'Slideshare', 'created': u'Tue Mar 27 10:10:11 -0500 2012'})
+
+    def test_extract_aliases(self):
+        # ensure that the dryad reader can interpret an xml doc appropriately
+        f = open(SAMPLE_EXTRACT_ALIASES_PAGE, "r")
+        aliases = self.provider._extract_aliases(f.read())
+        assert_equals(aliases, [('title', u'Manufacturing Serendipity')])
+
+    def test_extract_metrics_success(self):
+        f = open(SAMPLE_EXTRACT_METRICS_PAGE, "r")
+        metrics_dict = self.provider._extract_metrics(f.read())
+        assert_equals(metrics_dict["slideshare:views"], 337)
+        assert_equals(metrics_dict["slideshare:downloads"], 4)
+
+
---
+++ totalimpact/providers/slideshare.py
@@ -0,0 +1,139 @@
+from totalimpact.providers import provider
+from totalimpact.providers.provider import Provider, ProviderContentMalformedError
+from totalimpact.providers.secrets import Slideshare_key, Slideshare_secret
+
+import simplejson, urllib, time, hashlib, re
+from xml.dom import minidom 
+from xml.parsers.expat import ExpatError
+
+import logging
+logger = logging.getLogger('providers.slideshare')
+
+class Slideshare(Provider):  
+
+    example_id = ("url", "http://www.slideshare.net/cavlec")
+    url = "http://www.slideshare.net/"
+    descr = "The best way to share presentations, documents and professional videos."
+
+    member_items_url_template = "http://www.slideshare.net/api/2/get_slideshows_by_user?api_key=" + Slideshare_key + "&detailed=1&ts=%s&hash=%s&username_for=%s"
+    everything_url_template = "http://www.slideshare.net/api/2/get_slideshow?api_key=" + Slideshare_key + "&detailed=1&ts=%s&hash=%s&slideshow_url=%s"
+    biblio_url_template = everything_url_template
+    aliases_url_template = everything_url_template
+    metrics_url_template = everything_url_template
+    provenance_url_template = "%s"
+
+    sanity_check_re = re.compile("<Slideshow")
+
+    static_meta_dict = {
+        "downloads": {
+            "display_name": "downloads",
+            "provider": "Slideshare",
+            "provider_url": "http://www.slideshare.net/",
+            "description": "The number of times the presentation has been downloaded",
+            "icon": "http://www.slideshare.net/favicon.ico" ,
+        },    
+        "favorites": {
+            "display_name": "favorites",
+            "provider": "Slideshare",
+            "provider_url": "http://www.slideshare.net/",
+            "description": "The number of times the presentation has been favorited",
+            "icon": "http://www.slideshare.net/favicon.ico" ,
+        },    
+        "comments": {
+            "display_name": "comments",
+            "provider": "Slideshare",
+            "provider_url": "http://www.slideshare.net/",
+            "description": "The number of comments the presentation has received",
+            "icon": "http://www.slideshare.net/favicon.ico" ,
+        },    
+        "views": {
+            "display_name": "views",
+            "provider": "Slideshare",
+            "provider_url": "http://www.slideshare.net/",
+            "description": "The number of times the presentation has been viewed",
+            "icon": "http://www.slideshare.net/favicon.ico" ,
+        }    
+    }
+
+
+    def __init__(self):
+        super(Slideshare, self).__init__()
+
+    def is_relevant_alias(self, alias):
+        (namespace, nid) = alias
+        relevant = ((namespace=="url")and ("slideshare.net" in nid))
+        return(relevant)
+
+    #override because need to break up id
+    def _get_templated_url(self, template, id, method=None):
+        if method=="provenance":
+            return template %id
+            
+        ts = time.time()
+        hash_combo = hashlib.sha1(Slideshare_secret + str(ts)).hexdigest()
+        complete_url = template %(ts, hash_combo, id)
+        return(complete_url)
+
+    def _sanity_check_page(self, page):
+        if not self.sanity_check_re.search(page):
+            raise ProviderContentMalformedError
+        return True
+
+    def _extract_members(self, page, query_string): 
+        try:
+            doc = minidom.parseString(page.strip().encode('utf-8'))
+        except ExpatError:
+            raise ProviderContentMalformedError
+
+        self._sanity_check_page(page)
+
+        urls = doc.getElementsByTagName("URL")
+        members = [("url", url.firstChild.data) for url in list(set(urls))]
+        return(members)
+
+    def _extract_biblio(self, page, id=None):
+        dict_of_keylists = {
+            'title' : ['Slideshow', 'Title'],
+            'username' : ['Slideshow', 'Username'],
+            'created' : ['Slideshow', 'Created'],
+        }
+        self._sanity_check_page(page)
+
+        biblio_dict = provider._extract_from_xml(page, dict_of_keylists)
+        biblio_dict["repository"] = "Slideshare"
+        return biblio_dict    
+       
+    def _extract_aliases(self, page, id=None):
+        dict_of_keylists = {
+            'title' : ['Slideshow', 'Title']
+        }
+        self._sanity_check_page(page)
+
+        aliases_dict = provider._extract_from_xml(page, dict_of_keylists)
+        if aliases_dict:
+            aliases_list = [(namespace, nid) for (namespace, nid) in aliases_dict.iteritems()]
+        else:
+            aliases_list = []
+        return aliases_list
+
+
+    def _extract_metrics(self, page, status_code=200, id=None):
+        if status_code != 200:
+            if status_code == 404:
+                return {}
+            else:
+                raise(self._get_error(status_code))
+        self._sanity_check_page(page)
+
+        dict_of_keylists = {
+            'slideshare:downloads' : ['Slideshow', 'NumDownloads'],
+            'slideshare:views' : ['Slideshow', 'NumViews'],
+            'slideshare:comments' : ['Slideshow', 'NumComments'],
+            'slideshare:favorites' : ['Slideshow', 'NumFavorites'],
+        }
+
+        metrics_dict = provider._extract_from_xml(page, dict_of_keylists)
+        for mykey in metrics_dict:
+            metrics_dict[mykey] = int( metrics_dict[mykey])
+        return metrics_dict
+
