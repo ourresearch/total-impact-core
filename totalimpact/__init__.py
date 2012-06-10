5ed95c62e31e6cafd3c0caf0f421e95c3db13e66
Jason Priem <jp@jasonpriem.org>
2012-06-10 03:45:09 -0400

    More logging simplification

--- totalimpact/__init__.py
+++ totalimpact/__init__.py
@@ -1,13 +1,16 @@
 from totalimpact import default_settings
-import os, logging
+import os, logging, sys
 from flask import Flask
 
-# see http://docs.python.org/howto/logging-cookbook.html
+# see http://wiki.pylonshq.com/display/pylonscookbook/Alternative+logging+configuration
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format='"%(asctime)s %(levelname)8s %(name)s - %(message)s"',
+    datefmt='%H:%M:%S'
+)
+
 logger = logging.getLogger("ti")
-ch = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s %(levelname)8s %(name)s - %(message)s")
-ch.setFormatter(formatter)
-logger.addHandler(ch)
 
 app = Flask(__name__)
 app.config.from_object(default_settings)
