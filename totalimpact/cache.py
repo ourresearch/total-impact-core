523c0b188990f1a76d3a9146945edd849cf510f4
Jason Priem <jason@jasonpriem.com>
2012-06-25 00:21:59 -0500

    removes experimental cache code

--- totalimpact/cache.py
+++ totalimpact/cache.py
@@ -1,10 +1,3 @@
-import hashlib
-import memcache
-from cPickle import UnpickleableError
-from totalimpact.tilogging import logging
-
-# set up logging
-logger = logging.getLogger(__name__)
 
 class CacheException(Exception):
     pass
@@ -16,23 +9,14 @@ class Cache(object):
         self.max_cache_age = max_cache_age
 
     def get_cache_entry(self, url):
-        """ Get an entry from the cache, returns None if not found """
-        key = hashlib.md5(url).hexdigest()
-        mc = memcache.Client(['127.0.0.1:11211'])
-        return mc.get(key)
+
+        return None
+
 
     def set_cache_entry(self, url, data):
-        """ Store a cache entry """
-        key = hashlib.md5(url).hexdigest()
-        mc = memcache.Client(['127.0.0.1:11211'])
-        try:
-            set_response = mc.set(key, data, time=self.max_cache_age)
-            if not set_response:
-                raise CacheException("Unable to store into Memcached. Make sure memcached server is running.")
-        except UnpickleableError:
-            # This happens when trying to cache a thread.lock object, for example.  Just don't cache.
-            logger.debug("In set_cache_entry with " + url + " but got Unpickleable Error")
-            set_response = None
-        return (set_response)
+
+        return True
+
+  
         
         
