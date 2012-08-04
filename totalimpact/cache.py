dbbaf81f2542d74c71ac5e07a42560dbd09b6608
Heather Piwowar <hpiwowar@gmail.com>
2012-08-03 22:54:55 -0700

    enable memcached http caching through heroku addon

--- totalimpact/cache.py
+++ totalimpact/cache.py
@@ -1,3 +1,11 @@
+import os
+import pylibmc
+import hashlib
+import logging
+from cPickle import UnpickleableError
+
+# set up logging
+logger = logging.getLogger("ti.cache")
 
 class CacheException(Exception):
     pass
@@ -9,14 +17,32 @@ class Cache(object):
         self.max_cache_age = max_cache_age
 
     def get_cache_entry(self, url):
-
-        return None
-
+        """ Get an entry from the cache, returns None if not found """
+        key = hashlib.md5(url).hexdigest()
+        mc = pylibmc.Client(
+		    servers=[os.environ.get('MEMCACHE_SERVERS')],
+		    username=os.environ.get('MEMCACHE_USERNAME'),
+		    password=os.environ.get('MEMCACHE_PASSWORD'),
+		    binary=True)
+        return mc.get(key)
 
     def set_cache_entry(self, url, data):
-
-        return True
-
+        """ Store a cache entry """
+        key = hashlib.md5(url).hexdigest()
+        mc = pylibmc.Client(
+		    servers=[os.environ.get('MEMCACHE_SERVERS')],
+		    username=os.environ.get('MEMCACHE_USERNAME'),
+		    password=os.environ.get('MEMCACHE_PASSWORD'),
+		    binary=True)
+        try:
+            set_response = mc.set(key, data, time=self.max_cache_age)
+            if not set_response:
+                raise CacheException("Unable to store into Memcached. Make sure memcached server is running.")
+        except UnpickleableError:
+            # This happens when trying to cache a thread.lock object, for example.  Just don't cache.
+            logger.debug("In set_cache_entry with " + url + " but got Unpickleable Error")
+            set_response = None
+        return (set_response)
   
         
         
