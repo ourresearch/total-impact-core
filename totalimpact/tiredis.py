d761b1b8d1c1c007875612099fa889176a102365
Jason Priem <jason@jasonpriem.com>
2012-08-08 15:32:24 -0700

    tiredis passes tests for its three update-tracking functions

--- totalimpact/tiredis.py
+++ totalimpact/tiredis.py
@@ -9,7 +9,27 @@ def set_num_providers_left(self, item_id, num_providers_left):
     ))
     self.set(item_id, num_providers_left)
 
+
+def get_num_providers_left(self, item_id):
+    r = self.get(item_id)
+    if r is None:
+        return None
+    else:
+        return int(r)
+
+def decr_num_providers_left(self, item_id, provider_name):
+    num_providers_left = self.decr(item_id)
+    logger.info("bumped providers_run with {provider_name} for {id}. {num_providers_left} left to run.".format(
+        provider_name=provider_name,
+        id=item_id,
+        num_providers_left=num_providers_left
+    ))
+    return int(num_providers_left)
+
+
 redis.Redis.set_num_providers_left = set_num_providers_left
+redis.Redis.get_num_providers_left = get_num_providers_left
+redis.Redis.decr_num_providers_left = decr_num_providers_left
 
 def from_url(url):
     r = redis.from_url(url)
