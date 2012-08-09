878649d64c2a7857e2255da6a09fc15adb8893a0
Jason Priem <jason@jasonpriem.com>
2012-08-08 20:46:18 -0700

    models.MemberItems.start_update makes sure update hasn't already started.

--- totalimpact/models.py
+++ totalimpact/models.py
@@ -189,6 +189,11 @@ class MemberItems():
         return ret
 
     def _update(self, pages, key):
+
+        # no need to run an update if one is underway or just finished.
+        if self.redis.get(key) is not None:
+            return True
+
         status = {
             "memberitems": [],
             "pages": len(pages),
