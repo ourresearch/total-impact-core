751ad2af21c89ddf5a791b60fa6e3bf7b59ef7ef
Heather Piwowar <hpiwowar@gmail.com>
2012-08-14 19:51:43 -0700

    add ip_address to collections and recent collection report

--- config/couch/views/latest-collections.js
+++ config/couch/views/latest-collections.js
@@ -4,6 +4,10 @@ function(doc) {
         if (doc.title.indexOf("[ti test]") == 0) {
             test = 1;
         }
-        emit([test, doc.created], [doc.title, doc.item_tiids.length])
+        var ip_address = ""
+        if (typeof doc.ip_address != "undefined") {
+            ip_address = doc.ip_address
+        }
+        emit([test, doc.created], [doc.title, doc.item_tiids.length, ip_address])
     }
 }
--- test/unit_tests/test_views.py
+++ test/unit_tests/test_views.py
@@ -186,7 +186,7 @@ class TestCollection(ViewsTester):
         response_loaded = json.loads(response.data)
         assert_equals(
                 set(response_loaded.keys()),
-                set([u'created', u'item_tiids', u'last_modified', u'title', u'type', u'_id', u'_rev']))
+                set([u'created', u'item_tiids', u'last_modified', u'ip_address', u'title', u'type', u'_id', u'_rev']))
         assert_equals(len(response_loaded["_id"]), 6)
         assert_equals(response_loaded["item_tiids"], [u'tiid1', u'tiid2'])
 
@@ -251,7 +251,7 @@ class TestCollection(ViewsTester):
         print resp
         assert_equals(resp.status_code, 210)
         collection_data = json.loads(resp.data)
-        assert_equals(collection_data.keys(), [u'title', u'items', u'_rev', u'created', u'last_modified', u'_id', u'type'])
+        assert_equals(set(collection_data.keys()), set([u'title', u'items', u'_rev', u'created', u'last_modified', u'ip_address', u'_id', u'type']))
 
         items_urls = [item_from_db['aliases']['url'][0] for item_from_db in collection_data["items"]]
         expected_ids = [i[1] for i in items]
@@ -282,7 +282,7 @@ class TestCollection(ViewsTester):
         print resp
         assert_equals(resp.status_code, 200)
         collection_data = json.loads(resp.data)
-        assert_equals(set(collection_data.keys()), set([u'title', u'item_tiids', u'_rev', u'created', u'last_modified', u'_id', u'type']))
+        assert_equals(set(collection_data.keys()), set([u'title', u'item_tiids', u'_rev', u'created', u'last_modified', u'ip_address', u'_id', u'type']))
         assert_equals(collection_data["item_tiids"], tiids)
 
     def test_collection_update_puts_items_on_update_queue(self):
