6af66bdc0c1e1b5258636a202a5af3339858329c
Heather Piwowar <hpiwowar@gmail.com>
2012-08-16 20:07:21 -0700

    add back older couch maint code into HEAD file

--- extras/couch_maint.py
+++ extras/couch_maint.py
@@ -15,24 +15,54 @@ couch = couchdb.Server(url=os.getenv("CLOUDANT_URL"))
 db = couch[os.getenv("CLOUDANT_DB")]
 logger.info("connected to couch/" + os.getenv("CLOUDANT_DB"))
 
-view_name = "admin/old-wikipedia-snaps"
+#wikipedia_snap_cleanup()
+#bad_doi_cleanup()
 
-for row in db.view(view_name, include_docs=True):
+def wikipedia_snap_cleanup():
+    view_name = "admin/old-wikipedia-snaps"
 
-    doc = row.doc
-    logger.info("got doc '{id}' back from {view_name}, with drilldown_url {url}".format(
-        id=row.id,
-        view_name=view_name,
-        url=doc["drilldown_url"]
+    for row in db.view(view_name, include_docs=True):
 
-    ))
-    logger.info("deleting doc '{id}'.".format(
-        id=row.id
-    ))
-    db.delete(doc)
+        doc = row.doc
+        logger.info("got doc '{id}' back from {view_name}, with drilldown_url {url}".format(
+            id=row.id,
+            view_name=view_name,
+            url=doc["drilldown_url"]
 
-logger.info("finished the update.")
+        ))
+        logger.info("deleting doc '{id}'.".format(
+            id=row.id
+        ))
+        db.delete(doc)
+
+    logger.info("finished the update.")
 
 
+def bad_doi_cleanup():
+    view_name = "admin/bad_dois"
+
+    for row in db.view(view_name, include_docs=True):
+
+        doc = row.doc
+        logger.info("got doc '{id}' back from {view_name}".format(
+            id=row.id,
+            view_name=view_name
+        ))
+
+        logger.info("doc '{id}' has these doi aliases: {aliases}".format(
+            id=row.id,
+            aliases=doc["aliases"]["doi"]
+        ))
+
+        good_dois = [x for x in doc["aliases"]["doi"] if " []" not in x]
+        doc["aliases"]["doi"] = good_dois
+
+        logger.info("saving doc '{id}', which now has these aliases: '{aliases}'".format(
+            id=row.id,
+            aliases=doc["aliases"]
+        ))
+        db.save(doc)
+
+    logger.info("finished the update.")
 
 
