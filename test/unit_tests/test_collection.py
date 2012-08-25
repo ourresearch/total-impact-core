8577ff6ad8e8e6bdaa3f6fa07ca670c6260bc362
Heather Piwowar <hpiwowar@gmail.com>
2012-08-25 13:32:21 -0700

    reorder functions, better return values

--- test/unit_tests/test_collection.py
+++ test/unit_tests/test_collection.py
@@ -72,28 +72,38 @@ class TestCollection():
         expected = {u'dryad:package_views': [537, 0, 0, 0], u'wikipedia:mentions': [1, 0, 0, 0], u'dryad:most_downloaded_file': [70, 0, 0, 0], u'mendeley:readers': [57, 52, 13, 0], u'dryad:total_downloads': [114, 0, 0, 0], u'mendeley:groups': [4, 4, 1, 0]}
         assert_equals(response, expected)
 
-    def test_calc_table(self):
+    def test_calc_table_internals(self):
         # from http://www.milefoot.com/math/stat/ci-medians.htm
-        response = collection.calc_table(9, thresh=0.80, p_range=[50])
+        response = collection.calc_confidence_interval_table(9, 0.80, [50])
         assert_equals(response[0], 0.8203125)
-        assert_equals(response[1], (3,7))
+        assert_equals(response[1][50], (3,7))
 
         # from https://onlinecourses.science.psu.edu/stat414/book/export/html/231
-        response = collection.calc_table(9, thresh=0.90, p_range=[50])
+        response = collection.calc_confidence_interval_table(9, 0.90, [50])
         assert_equals(response[0], 0.9609375)
-        assert_equals(response[1], (2,8))
+        assert_equals(response[1][50], (2,8))
 
         # from https://onlinecourses.science.psu.edu/stat414/book/export/html/231
-        response = collection.calc_table(14, thresh=0.90, p_range=[50])
+        response = collection.calc_confidence_interval_table(14, 0.90, [50])
         assert_equals(response[0], 0.942626953125)
-        assert_equals(response[1], (4,11))
+        assert_equals(response[1][50], (4,11))
 
-        response = collection.calc_table(9, thresh=0.95, p_range=[90])
+    def test_calc_table_extremes(self):
+        response = collection.calc_confidence_interval_table(9, 0.95, [90])
         assert_equals(response[0], 0.9916689060000001)
-        assert_equals(response[1], (6,10))
+        assert_equals(response[1][90], (6,10))
 
-        response = collection.calc_table(9, thresh=0.95, p_range=[10])
+        response = collection.calc_confidence_interval_table(9, 0.95, [10])
         assert_equals(response[0], 0.9916689060000002)
-        assert_equals(response[1], (0,4))
+        assert_equals(response[1][10], (0,4))
 
+    def test_calc_table(self):
+        response = collection.calc_confidence_interval_table(9, 0.95, [i*10 for i in range(10)])
+        print response
+        expected = [(10, 30), (10, 40), (10, 60), (10, 60), (20, 70), (30, 80), (40, 90), (50, 90), (60, 90), (70, 90)]
+        assert_equals(response[2], expected)
 
+        response = collection.calc_confidence_interval_table(50, 0.95, range(100))
+        print response
+        expected = [(1, 9), (1, 13), (1, 15), (2, 17), (3, 21), (5, 23), (6, 25), (7, 27), (8, 29), (10, 33), (12, 35), (13, 37), (14, 39), (16, 41), (18, 43), (20, 45), (21, 47), (22, 49), (24, 50), (26, 52), (28, 54), (30, 56), (32, 58), (33, 60), (34, 62), (36, 64), (38, 66), (40, 67), (42, 68), (44, 70), (46, 72), (48, 74), (50, 76), (51, 78), (53, 79), (55, 80), (57, 82), (59, 84), (61, 86), (63, 87), (65, 88), (67, 90), (71, 92), (73, 93), (75, 94), (77, 95), (79, 97), (83, 98), (85, 99), (87, 99), (91, 99)]
+        assert_equals(response[2], expected)
--- totalimpact/collection.py
+++ totalimpact/collection.py
@@ -10,7 +10,6 @@ import logging
 logger = logging.getLogger('ti.collection')
 
 def make(owner=None):
-
     key, key_hash = _make_update_keypair()
 
     now = datetime.datetime.now().isoformat()
@@ -48,66 +47,12 @@ def _make_update_keypair():
     key_hash = generate_password_hash(key)
     return key, key_hash
 
-# from http://userpages.umbc.edu/~rcampbel/Computers/Python/probstat.html
-def choose(n, k):
-    return 1 if (k == 0) else n*choose(n-1, k-1)/k
-
-def probPercentile(p, n, i):
-    prob = choose(n, i) * p**i * (1-p)**(n-i)
-    return(prob)
-
-# p range is p values * 100
-def calc_table(n, thresh=0.95, p_range=range(8, 97, 2)):
-    ps_min = [None for i in range(n+1)]
-    ps_max = [None for i in range(n+1)]
-    limits = None
-    for p in p_range:
-        print(p)
-        out = {}
-        for i in range(0, n+1):
-            out[i] = probPercentile(p*0.01, n, i)
-        print(out)
-        mymax = [(i, out[i]) for i in out if out[i]==max(out.values())]
-        mymaxA = min([i for (i, val) in mymax])
-        mymaxB = max([i for (i, val) in mymax])
-        for i in range(0, n/2):
-            myrange = range(max(0, (mymaxA-i)), min(len(out), (1+mymaxB+i)))
-            print "lenout", len(out)
-            print min(len(out), (mymaxB+i))
-            print mymaxB+i
-            print myrange
-
-            mysum = sum([out[j] for j in myrange])
-            if mysum >= thresh:
-                print mysum
-                limits = (min(myrange), 1+max(myrange))
-                print "from index", limits[0], "to (but not including)", limits[1]
-                for i in myrange:
-                    ps_max[i-1] = p
-                    if not ps_min[i-1]:
-                        ps_min[i-1] = p
-                break
-    #for i in range(n-1, 0, -1):
-    #    print (i+0.0)/n, ps_min[i], ps_max[i]
-    return(mysum, limits, zip(ps_min, ps_max))
-
-def get_normalization_numbers(items):
-    metric_value_lists = get_metric_value_lists(items)
-    metrics_to_normalize = metric_value_lists.keys()
-    for key in metrics_to_normalize:
-        if ("plosalm" in key):
-            del metric_value_lists[key]
-        elif not isinstance(metric_value_lists[key][0], int):
-            del metric_value_lists[key]
-    return metric_value_lists  
-
 def clean_value_for_csv(value_to_store):
     try:
         value_to_store = value_to_store.encode("utf-8").strip()
     except AttributeError:
         pass
     return value_to_store
-
 
 def make_csv_rows(items):
     header_metric_names = []
@@ -141,6 +86,70 @@ def make_csv_rows(items):
         rows += [ordered_fieldnames]
     return(ordered_fieldnames, rows)
 
+def make_csv_stream(items):
+    (header, rows) = make_csv_rows(items)
+
+    mystream = StringIO.StringIO()
+    dw = csv.DictWriter(mystream, delimiter=',', dialect=csv.excel, fieldnames=header)
+    dw.writeheader()
+    for row in rows:
+        dw.writerow(row)
+    contents = mystream.getvalue()
+    mystream.close()
+    return contents
+
+
+# from http://userpages.umbc.edu/~rcampbel/Computers/Python/probstat.html
+def choose(n, k):
+    return 1 if (k == 0) else n*choose(n-1, k-1)/k
+
+def probPercentile(p, n, i):
+    prob = choose(n, i) * p**i * (1-p)**(n-i)
+    return(prob)
+
+def calc_confidence_interval_table(
+        n,                      # sample size
+        ci_thresh=0.95,         # confidence interval threshold
+        percentiles=range(8, 97, 2) # percentiles to calculate.  Median==50.
+        ):
+    ps_min = [None for i in range(n+1)]
+    ps_max = [None for i in range(n+1)]
+    limits = {}
+    for p in percentiles:
+        print(p)
+        out = {}
+        for i in range(0, n+1):
+            out[i] = probPercentile(p*0.01, n, i)
+        print(out)
+        mymax = [(i, out[i]) for i in out if out[i]==max(out.values())]
+        mymaxA = min([i for (i, val) in mymax])
+        mymaxB = max([i for (i, val) in mymax])
+        for i in range(0, n/2):
+            myrange = range(max(0, (mymaxA-i)), min(len(out), (1+mymaxB+i)))
+            mysum = sum([out[j] for j in myrange])
+            if mysum >= ci_thresh:
+                print mysum
+                limits[p] = (min(myrange), 1+max(myrange))
+                print "from index", limits[p][0], "to (but not including)", limits[p][1]
+                for i in myrange:
+                    ps_max[i] = p
+                    if not ps_min[i]:
+                        ps_min[i] = p
+                break
+    #for i in range(n-1, 0, -1):
+    #    print (i+0.0)/n, ps_min[i], ps_max[i]
+    return(mysum, limits, zip(ps_min, ps_max))
+
+def get_normalization_numbers(items):
+    metric_value_lists = get_metric_value_lists(items)
+    metrics_to_normalize = metric_value_lists.keys()
+    for key in metrics_to_normalize:
+        if ("plosalm" in key):
+            del metric_value_lists[key]
+        elif not isinstance(metric_value_lists[key][0], int):
+            del metric_value_lists[key]
+    return metric_value_lists  
+
 def get_metric_value_lists(items):
     (ordered_fieldnames, rows) = make_csv_rows(items)
     metric_values = {}
@@ -153,14 +162,4 @@ def get_metric_value_lists(items):
             metric_values[metric_name] = sorted(values, reverse=True)
     return metric_values
 
-def make_csv_stream(items):
-    (header, rows) = make_csv_rows(items)
 
-    mystream = StringIO.StringIO()
-    dw = csv.DictWriter(mystream, delimiter=',', dialect=csv.excel, fieldnames=header)
-    dw.writeheader()
-    for row in rows:
-        dw.writerow(row)
-    contents = mystream.getvalue()
-    mystream.close()
-    return contents
