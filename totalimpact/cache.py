c7c4cafcf854f6aa9eafa3a7f85c1ebe68331a91
Heather Piwowar <hpiwowar@gmail.com>
2012-09-03 21:32:38 -0700

    retry on memcached errors

--- totalimpact/cache.py
+++ totalimpact/cache.py
@@ -4,6 +4,8 @@ import hashlib
 import logging
 from cPickle import UnpickleableError
 
+from totalimpact.utils import Retry
+
 # set up logging
 logger = logging.getLogger("ti.cache")
 
@@ -16,6 +18,7 @@ class Cache(object):
     def __init__(self, max_cache_age=86400):
         self.max_cache_age = max_cache_age
 
+    @Retry(3, pylibmc.Error, 0.1)
     def get_cache_entry(self, url):
         """ Get an entry from the cache, returns None if not found """
         key = hashlib.md5(url.encode("utf-8")).hexdigest()
@@ -26,6 +29,7 @@ class Cache(object):
 		    binary=True)
         return mc.get(key)
 
+    @Retry(3, pylibmc.Error, 0.1)
     def set_cache_entry(self, url, data):
         """ Store a cache entry """
         key = hashlib.md5(url.encode("utf-8")).hexdigest()
@@ -40,7 +44,7 @@ class Cache(object):
                 raise CacheException("Unable to store into Memcached. Make sure memcached server is running.")
         except UnpickleableError:
             # This happens when trying to cache a thread.lock object, for example.  Just don't cache.
-            logger.debug("In set_cache_entry with " + url + " but got Unpickleable Error")
+            logger.debug("In set_cache_entry with " + url + " but got Error")
             set_response = None
         return (set_response)
   
