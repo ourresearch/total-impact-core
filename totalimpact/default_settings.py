8397ef0319f31b5348d832b2bcf63b90be06deb9
Heather Piwowar <hpiwowar@gmail.com>
2012-09-08 06:31:41 -0700

    we've got blogs!

--- totalimpact/default_settings.py
+++ totalimpact/default_settings.py
@@ -3,8 +3,8 @@
 # ALL KEYS HAVE TO BE UPPERCASE TO BE STORED IN APP SETTINGS
 #
 
-USER_AGENT = "TotalImpact/0.2.0" # User-Agent string to use on HTTP requests
-VERSION = "jean-claude" # TI version
+USER_AGENT = "TotalImpact/0.3.0" # User-Agent string to use on HTTP requests
+VERSION = "bruce" # TI version
 PROXY = "" # used with  providers-test-proxy.py script in the extras directory
 CACHE_ENABLED = True # Memcache server enabled
 
@@ -12,27 +12,28 @@ CACHE_ENABLED = True # Memcache server enabled
 # Alias methods will be called in the order of this list
 PROVIDERS = [
     # this is up here because it can produce dois
-    ("pubmed", { "workers":1 }),  # 1 because rate limited
+    ("pubmed", {}),  # 1 because rate limited
 
     # best biblio providers go here, in order with best first
-    ("crossref", { "workers":3 }),
-    ("dryad", { "workers":3 }),            
-    ("github", { "workers":3 }),
-    ("slideshare", { "workers":3 }),
+    ("crossref", {}),
+    ("dryad", {}),            
+    ("github", {}),
+    ("slideshare", {}),
 
     # if-need-be biblio providers go here, in order with best first
-    ("mendeley", { "workers":3 }),
-    ("bibtex", { "workers":3 }),
-    ("dataone", {  "workers":3 }),
-    ("webpage", {  "workers":3 }),
+    ("mendeley", {}),
+    ("bibtex", {}),
+    ("dataone", {}),
+    ("webpage", {}),
 
     # don't-have-biblio providers go here, alphabetical order
-    ("citeulike", { "workers":3 }),   
-    ("delicious", { "workers":3 }),   
-    ("facebook", { "workers":3 }),   
-    ("plosalm", { "workers":3 }),
-    # ("researchblogging", { "workers":3 }), # takes too long for interactive test
-    ("topsy", { "workers":3 }),
-    ("wikipedia", { "workers":3 }),
+    ("citeulike", {}),   
+    ("delicious", {}),   
+    ("facebook", {}),   
+    ("plosalm", {}),
+    ("scienceseeker", {}),
+    # ("researchblogging", {}), # takes too long
+    ("topsy", {}),
+    ("wikipedia", {}),
 ]
 
