d31ab8f97085153fd1efc9ef358784011fa00cd9
Heather Piwowar <hpiwowar@gmail.com>
2012-09-13 11:02:39 -0700

    fix bad merge

--- extras/couch_maint.py
+++ extras/couch_maint.py
@@ -325,7 +325,7 @@ confirm = None
 confirm = raw_input("\nType YES if you are sure you want to run this test:")
 if confirm=="YES":
     ### call the function here
-    put_snaps_in_items("000000000000", "001")
+    put_snaps_in_items()
 else:
     print "nevermind, then."
 
