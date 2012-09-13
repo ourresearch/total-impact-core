e1b5d89ca44ddf1badbbf6af247f47aee890f3da
Heather Piwowar <hpiwowar@gmail.com>
2012-09-12 18:02:25 -0700

    handle key error, update unit test to new metrics format

--- totalimpact/models.py
+++ totalimpact/models.py
@@ -75,7 +75,10 @@ class ItemFactory():
         for metric_name in metrics:
             if metric_name in cls.all_static_meta.keys():  # make sure we still support this metrics type
                 #delete the raw history from what we return to the client for now
-                del metrics[metric_name]["values"]["raw_history"]
+                try:
+                    del metrics[metric_name]["values"]["raw_history"]
+                except KeyError:
+                    pass
 
                 # add static data
                 metrics[metric_name]["static_meta"] = cls.all_static_meta[metric_name]            
