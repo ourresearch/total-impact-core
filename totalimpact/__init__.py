b9033079d3e2d765e0d6bb66b91bca9b3e864c33
Heather Piwowar <hpiwowar@gmail.com>
2012-11-16 21:15:28 -0800

    update log format

--- totalimpact/__init__.py
+++ totalimpact/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 logging.basicConfig(
     stream=sys.stdout,
     level=logging.DEBUG,
-    format='%(threadName)25s %(levelname)8s %(name)s - %(message)s'
+    format='%(levelname)8s %(threadName)30s %(name)s - %(message)s'
 )
 
 logger = logging.getLogger("ti")
