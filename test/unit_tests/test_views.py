d62566c5f11eabcac2f7f67eefb08682fa67d62b
Jason Priem <jp@jasonpriem.org>
2012-11-18 12:39:06 -0500

    ViewsTester uploads a test item to the disposable CouchDB instance it creates.

--- test/unit_tests/test_views.py
+++ test/unit_tests/test_views.py
@@ -49,11 +49,93 @@ class ViewsTester(unittest.TestCase):
         pass
 
     def setUp(self):
+        test_item = '''
+            {
+            "_id": "1aff9dfebea711e1bdf912313d1a5e63",
+            "_rev": "968-c7891982fca2ea41346a20b80c2b888d",
+            "aliases": {
+                "created": "2012-06-25T09:21:11.960306",
+                "doi": [
+                    "10.5061/dryad.j1fd7"
+                ],
+                "last_modified": "2012-07-29T02:00:29.001625",
+                "title": [
+                    "Data from: Data archiving is a good use of research funds",
+                    "data from: data archiving is a good  investment"
+                ],
+                "url": [
+                    "http://datadryad.org/handle/10255/dryad.33537",
+                    "http://hdl.handle.net/10255/dryad.33537"
+                ]
+            },
+            "biblio": {
+                "authors": "Piwowar, Vision, Whitlock, Piwowar, Vision, Whitlock, Piwowar, Vision, Whitlock",
+                "genre": "dataset",
+                "h1": "Data from: Data archiving is a good  investment",
+                "repository": "Dryad Digital Repository",
+                "title": "Data from: Data archiving is a good  investment",
+                "year": "2011"
+            },
+            "created": "2012-06-25T09:21:11.960271",
+            "currently_updating": false,
+            "last_modified": "2012-11-18T04:57:40.539053",
+            "metrics": {
+                "delicious:bookmarks": {
+                    "provenance_url": "http://www.delicious.com/url/4794ddb7a3e934ba23165af65fcfa9cd",
+                    "static_meta": {
+                        "description": "The number of bookmarks to this artifact (maximum=100).",
+                        "display_name": "bookmarks",
+                        "icon": "http://www.delicious.com/favicon.ico",
+                        "provider": "Delicious",
+                        "provider_url": "http://www.delicious.com/"
+                    },
+                    "values": {
+                        "dryad": {
+                            "CI95_lower": 97,
+                            "CI95_upper": 100,
+                            "estimate_lower": 100,
+                            "estimate_upper": 100
+                        },
+                        "raw": 1
+                    }
+                },
+                "dryad:most_downloaded_file": {
+                    "provenance_url": "http://dx.doi.org/10.5061/dryad.j1fd7",
+                    "values": {
+                        "raw": 128,
+                        "raw_history": {
+                            "2012-06-25T09:21:16.117375": 103,
+                            "2012-06-26T18:05:19.655009": 103,
+                            "2012-06-26T20:10:16.912462": 103
+                        }
+                    }
+                }
+            },
+            "providersRunCounter": 10,
+            "providersWithMetricsCount": 10,
+            "providers_run": [
+                "Slideshare",
+                "Github",
+                "Plosalm",
+                "Facebook",
+                "Topsy",
+                "Citeulike",
+                "Wikipedia",
+                "Delicious",
+                "Mendeley",
+                "Dryad"
+            ],
+            "type": "item"
+        }
+        '''
+
         # hacky way to delete the "ti" db, then make it fresh again for each test.
         temp_dao = dao.Dao("http://localhost:5984", os.getenv("CLOUDANT_DB"))
         temp_dao.delete_db(os.getenv("CLOUDANT_DB"))
         self.d = dao.Dao("http://localhost:5984", os.getenv("CLOUDANT_DB"))
         self.d.update_design_doc()
+
+        self.d.save(json.loads(test_item))
 
         # do the same thing for the redis db.  We're using DB 8 for unittests.
         self.r = tiredis.from_url("redis://localhost:6379", db=8)
@@ -200,8 +282,8 @@ class TestItems(ViewsTester):
         )
         new_coll = json.loads(resp2.data)["collection"]
 
-        # 3 new items + 1 new item + 3 design docs + 2 collections
-        assert_equals(self.d.db.info()["doc_count"], 9)
+        # 3 new items + 1 new item + 3 design docs + 2 collections + one test_item
+        assert_equals(self.d.db.info()["doc_count"], 10)
 
 
 
