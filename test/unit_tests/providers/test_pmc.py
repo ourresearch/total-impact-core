244829607221809e9b3c54049f842a2339ef2f94
Heather Piwowar <hpiwowar@gmail.com>
2012-11-29 07:56:07 -0800

    add database testing infrastructure to pmc

--- test/unit_tests/providers/test_pmc.py
+++ test/unit_tests/providers/test_pmc.py
@@ -1,10 +1,11 @@
+import os, collections, simplejson
+
 from test.unit_tests.providers import common
 from test.unit_tests.providers.common import ProviderTestCase
 from totalimpact.providers.provider import Provider, ProviderContentMalformedError
+from totalimpact import dao
 from test.utils import http
 
-import os
-import collections
 from nose.tools import assert_equals, raises, nottest
 
 datadir = os.path.join(os.path.split(__file__)[0], "../../../extras/sample_provider_pages/pmc")
@@ -21,6 +22,30 @@ class TestPmc(ProviderTestCase):
 
     def setUp(self):
         ProviderTestCase.setUp(self)
+
+        # hacky way to delete the "ti" db, then make it fresh again for each test.
+        temp_dao = dao.Dao("http://localhost:5984", os.getenv("CLOUDANT_DB"))
+        temp_dao.delete_db(os.getenv("CLOUDANT_DB"))
+        self.d = dao.Dao("http://localhost:5984", os.getenv("CLOUDANT_DB"))
+        self.d.update_design_doc()
+
+        sample_data_dump = open(SAMPLE_EXTRACT_METRICS_PAGE, "r").read()
+
+        test_monthly_data = [
+            {"_id": "abc", 
+                "type": "provider_data_dump", 
+                "provider": "pmc", 
+                "raw": simplejson.dumps(sample_data_dump),
+                "provider_raw_version": 1.0,
+                "created": "2012-11-29T07:34:01.126892",
+                "aliases": {"pmid":["111", "222"]},
+                "min_event_date": "2012-10-01T07:34:01.126892",
+                "max_event_date": "2012-10-31T07:34:01.126892"
+            }
+        ]
+        print test_monthly_data
+        for doc in test_monthly_data:
+            self.d.db.save(doc)
 
     def test_is_relevant_alias(self):
         # ensure that it matches an appropriate ids
