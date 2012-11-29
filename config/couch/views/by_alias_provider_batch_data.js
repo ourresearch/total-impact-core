b2f10bed729bc386fc514b8d7b3347fed88ef614
Heather Piwowar <hpiwowar@gmail.com>
2012-11-29 13:51:54 -0800

    basic pmc working in unittests
    though doesn't handle different month data properly yet

---
+++ config/couch/views/by_alias_provider_batch_data.js
@@ -0,0 +1,19 @@
+function(doc) {
+    // lists aliases with provider batch data
+    if (doc.type == "provider_data_dump") {
+        // emit one or more rows for every namespace in aliases
+        for (var namespace in doc.aliases) {
+            namespaceid_list = doc.aliases[namespace];
+
+            // if just a single value, put it in a list
+            if (typeof namespaceid_list == "string") {
+                  namespaceid_list = new Array(namespaceid_list);
+            }
+
+            // emit a row for every id in the namespace id list except meta
+            for (var i in namespaceid_list) {
+                emit([doc.provider, [namespace, namespaceid_list[i]]], doc.max_event_date);
+            }
+        }
+    }
+}
