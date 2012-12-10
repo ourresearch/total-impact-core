5b445201625140c51ad6025000b593d23d5d6d30
Jason Priem <jp@jasonpriem.org>
2012-12-09 21:05:13 -0500

    passing create=true to GET v1/item creates it if it doesn't exist.
    So you shouldn't be able to get back a 404 if you include this param. The
    returned item will have a 210 code until it is done updating, just like it
    would if you had created it with the POST method. This breaks REST but is
    oh so useful.

---
+++ scratchpad.js
@@ -0,0 +1,77 @@
+{
+    "_id": "1aff9dfebea711e1bdf912313d1a5e63",
+    "_rev": "968-c7891982fca2ea41346a20b80c2b888d",
+    "aliases": {
+    "created": "2012-06-25T09:21:11.960306",
+        "doi": [
+        "10.5061/dryad.j1fd7"
+    ],
+        "last_modified": "2012-07-29T02:00:29.001625",
+        "title": [
+        "Data from: Data archiving is a good use of research funds",
+        "data from: data archiving is a good  investment"
+    ],
+        "url": [
+        "http://datadryad.org/handle/10255/dryad.33537",
+        "http://hdl.handle.net/10255/dryad.33537"
+    ]
+},
+    "biblio": {
+    "authors": "Piwowar, Vision, Whitlock, Piwowar, Vision, Whitlock, Piwowar, Vision, Whitlock",
+        "genre": "dataset",
+        "h1": "Data from: Data archiving is a good  investment",
+        "repository": "Dryad Digital Repository",
+        "title": "Data from: Data archiving is a good  investment",
+        "year": "2011"
+},
+    "created": "2012-06-25T09:21:11.960271",
+    "currently_updating": false,
+    "last_modified": "2012-11-18T04:57:40.539053",
+    "metrics": {
+    "delicious:bookmarks": {
+        "provenance_url": "http://www.delicious.com/url/4794ddb7a3e934ba23165af65fcfa9cd",
+            "static_meta": {
+            "description": "The number of bookmarks to this artifact (maximum=100).",
+                "display_name": "bookmarks",
+                "icon": "http://www.delicious.com/favicon.ico",
+                "provider": "Delicious",
+                "provider_url": "http://www.delicious.com/"
+        },
+        "values": {
+            "dryad": {
+                "CI95_lower": 97,
+                    "CI95_upper": 100,
+                    "estimate_lower": 100,
+                    "estimate_upper": 100
+            },
+            "raw": 1
+        }
+    },
+    "dryad:most_downloaded_file": {
+        "provenance_url": "http://dx.doi.org/10.5061/dryad.j1fd7",
+            "values": {
+            "raw": 128,
+                "raw_history": {
+                "2012-06-25T09:21:16.117375": 103,
+                "2012-06-26T18:05:19.655009": 103,
+                "2012-06-26T20:10:16.912462": 103
+            }
+        }
+    },
+},
+    "providersRunCounter": 10,
+    "providersWithMetricsCount": 10,
+    "providers_run": [
+    "Slideshare",
+    "Github",
+    "Plosalm",
+    "Facebook",
+    "Topsy",
+    "Citeulike",
+    "Wikipedia",
+    "Delicious",
+    "Mendeley",
+    "Dryad"
+],
+    "type": "item"
+}
