ea2524ddc8fe806602a3391dd73ec15222d549aa
Heather Piwowar <hpiwowar@gmail.com>
2012-12-12 18:13:38 -0800

    add active publisher updating through doi_prefixes_by_last_update_run view.  adds updater command line arg.  ADDS VIEW.
    now updates active_publisher doi prefix matches once per day, in batches throughout the day.

--- config/couch/views/items_by_last_modified.js
+++
@@ -1,6 +0,0 @@
-function(doc) {
-    // lists aliases by last_modified
-    if (doc.type == "item") {
-        emit(doc.last_modified, doc._id)
-    }
-}
--- totalimpact/updater.py
+++ totalimpact/updater.py
@@ -21,7 +21,7 @@ active_publishers = {
                 { 
                     "title": "eLife",
                     "issn": "2050-084X",
-                    "doi_prefix": "10.7554/eLife."
+                    "doi_prefix": "10.7554/eLife"
                 }
             ]
         },
@@ -31,27 +31,27 @@ active_publishers = {
                 {  
                     "title": "ZooKeys",
                     "issn": "1313-2970",
-                    "doi_prefix": "10.3897/zookeys."
+                    "doi_prefix": "10.3897/zookeys"
                 },
                 {  
                     "title": "PhytoKeys",
                     "issn": "1314-2003",
-                    "doi_prefix": "10.3897/phytokeys."
+                    "doi_prefix": "10.3897/phytokeys"
                 },
                 {  
                     "title": "BioRisk",
                     "issn": "1313-2652",
-                    "doi_prefix": "10.3897/biorisk."
+                    "doi_prefix": "10.3897/biorisk"
                 },
                 {  
                     "title": "NeoBiota",
                     "issn": "1314-2488",
-                    "doi_prefix": "10.3897/neobiota."
+                    "doi_prefix": "10.3897/neobiota"
                 },
                 {  
                     "title": "Journal of Hymenoptera Research",
                     "issn": "1314-2607",
-                    "doi_prefix": " 10.3897/JHR."
+                    "doi_prefix": " 10.3897/JHR"
                 }
             ]
         },
@@ -61,42 +61,42 @@ active_publishers = {
                 {  
                     "title": "Archaeology International",
                     "issn": "2048-4194",
-                    "doi_prefix": "10.5334/ai."
+                    "doi_prefix": "10.5334/ai"
                 },
                 {  
                     "title": "The Bulletin of the History of Archaeology",
                     "issn": "2047-6930",
-                    "doi_prefix": "10.5334/bha."
+                    "doi_prefix": "10.5334/bha"
                 },
                 {  
                     "title": "Stability: International Journal of Security and Development",
                     "issn": "2165-2627",
-                    "doi_prefix": "10.5334/sta."
+                    "doi_prefix": "10.5334/sta"
                 },
                 {  
                     "title": "Journal of Conservation and Museum Studies",
                     "issn": "1364-0429",
-                    "doi_prefix": "10.5334/jcms."
+                    "doi_prefix": "10.5334/jcms"
                 },
                 {  
                     "title": "Journal of Open Archaeology Data",
                     "issn": "2049-1565",
-                    "doi_prefix": "10.5334/data."  # but also some without a prefix
+                    "doi_prefix": "10.5334/data"  # but also some without a prefix
                 },
                 {  
                     "title": "Opticon1826",
                     "issn": "2049-8128",
-                    "doi_prefix": "10.5334/opt."
+                    "doi_prefix": "10.5334/opt"
                 },
                 {  
                     "title": "Papers from the Institute of Archaeology",
                     "issn": "2041-9015",
-                    "doi_prefix": "10.5334/pia."
+                    "doi_prefix": "10.5334/pia"
                 },
                 {  
                     "title": "Present Pasts",
                     "issn": "1759-2941",
-                    "doi_prefix": "10.5334/pp."
+                    "doi_prefix": "10.5334/pp"
                 },
 
             ]
@@ -104,45 +104,62 @@ active_publishers = {
     }
 
 
-def get_matching_dois_in_db(doi_prefix, mydao):
+def get_matching_dois_in_db(min_year, doi_prefix, mydao):
     db = mydao.db
-    dois = []
-    view_name = "queues/by_alias"
+    dois_to_update = []
+    docs_to_update = []
+    tiids_to_update = []
+
+    view_name = "doi_prefixes_by_last_update_run/doi_prefixes_by_last_update_run"
+    now = datetime.datetime.now()
+    yesterday = now - datetime.timedelta(days=1)
     view_rows = db.view(view_name, 
-            include_docs=False, 
-            startkey=["doi", doi_prefix.lower() + "00000000000"], 
-            endkey=["doi", doi_prefix.lower() + "zzzzzzzzzzzz"])
-    row_count = 0
-
+            include_docs=True, 
+            startkey=[doi_prefix.lower(), "00000000"], 
+            endkey=[doi_prefix.lower(), yesterday.isoformat()])
     for row in view_rows:
-        row_count += 1
-        doi = row.key[1]
-        if doi not in dois:
-            dois += [doi]
-        #logger.info("\n%s" % (doi))
-    return dois
+        doc = row.doc
+        tiid = doc["_id"]
+        #only update recent things for now
+        try:
+            year = doc["biblio"]["year"]
+        except KeyError:
+            continue
+        if year > min_year:
+            if tiid not in tiids_to_update:
+                docs_to_update += [doc]
+                tiids_to_update += [tiid]
 
-def update_dois_from_doi_prefix(doi_prefix, myredis, mydao):
-    dois = get_matching_dois_in_db(doi_prefix, mydao)
-    aliases = [("doi", doi) for doi in dois]
-    (old_and_new_tiids, new_items) = ItemFactory.create_or_find_items_from_aliases(aliases, myredis, mydao)
-    print old_and_new_tiids
-    number_to_sample = min(200, len(old_and_new_tiids))
-    tiids_to_update = random.sample(old_and_new_tiids, number_to_sample)
+            #logger.info("\n%s" % (doi))
+    return (tiids_to_update, docs_to_update)
+
+def get_least_recently_updated_items_from_doi_prefix(doi_prefix, myredis, mydao):
+    min_year = 2011  #only update 2012-2013 right now
+    (tiids_to_update, docs_to_update) = get_matching_dois_in_db(min_year, doi_prefix, mydao)
+    return (tiids_to_update, docs_to_update)
+
+def update_active_publisher_items(number_to_update, myredis, mydao):
+    all_tiids = []
+    all_docs = []
+    for publisher in active_publishers:
+        for journal_dict in active_publishers[publisher]["journals"]:
+            (tiids_from_doi_prefix, docs_from_doi_prefix) = get_least_recently_updated_items_from_doi_prefix(journal_dict["doi_prefix"], myredis, mydao)
+            logger.info("got {num_tiids} items for doi prefix {prefix}".format(
+                num_tiids=len(tiids_from_doi_prefix), prefix=journal_dict["doi_prefix"]))
+            all_tiids += tiids_from_doi_prefix
+            all_docs += docs_from_doi_prefix
+
+    print "all tiids, n=", len(all_tiids)
+    tiids_to_update = all_tiids[0:min(number_to_update, len(all_tiids))]
+    docs_to_update = all_docs[0:min(number_to_update, len(all_docs))]
+    response = update_docs_with_updater_timestamp(docs_to_update, mydao)        
+
+    print tiids_to_update
     QUEUE_DELAY_IN_SECONDS = 1.0
     mymixpanel.track("Trigger:Update", properties={"Number Items":len(tiids_to_update), "Update Type":"Scheduled Registered"}, ip=False)    
     ItemFactory.start_item_update(tiids_to_update, myredis, mydao, sleep_in_seconds=QUEUE_DELAY_IN_SECONDS)
-    return tiids_to_update
 
-def update_active_publisher_items(myredis, mydao):
-    all_tiids = []
-    for publisher in active_publishers:
-        for journal_dict in active_publishers[publisher]["journals"]:
-            tiids = update_dois_from_doi_prefix(journal_dict["doi_prefix"], myredis, mydao)
-            logger.info("sent update to {num_tiids} items for doi prefix {prefix}".format(
-                num_tiids=len(tiids), prefix=journal_dict["doi_prefix"]))
-            all_tiids += tiids
-    return all_tiids
+    return tiids_to_update
 
 def get_least_recently_updated_tiids_in_db(number_to_update, mydao):
     db = mydao.db
@@ -161,7 +178,7 @@ def update_docs_with_updater_timestamp(docs, mydao):
     now = datetime.datetime.now().isoformat()
     for doc in docs:
         doc["last_update_run"] = now
-    update_response = update_response = db.update(docs)
+    update_response = db.update(docs)
     print update_response
     return update_response
     
@@ -173,7 +190,9 @@ def update_least_recently_updated(number_to_update, myredis, mydao):
     ItemFactory.start_item_update(tiids_to_update, myredis, mydao, sleep_in_seconds=QUEUE_DELAY_IN_SECONDS)
     return tiids_to_update
 
-def main(run_publisher=False, run_least_recently_updated=True):
+def main(action_type, number_to_update=35):
+    #35 every 10 minutes is 35*6perhour*24hours=5040 per day
+
     cloudant_db = os.getenv("CLOUDANT_DB")
     cloudant_url = os.getenv("CLOUDANT_URL")
     redis_url = os.getenv("REDISTOGO_URL")
@@ -181,12 +200,12 @@ def main(run_publisher=False, run_least_recently_updated=True):
     mydao = dao.Dao(cloudant_url, cloudant_db)
     myredis = tiredis.from_url(redis_url)
 
-    number_to_update = 35  #35 every 10 minutes is 35*6perhour*24hours=5040 per day
-
     try:
-        if run_publisher:
-            tiids = update_active_publisher_items(myredis, mydao)
-        if run_least_recently_updated:
+        if action_type == "active_publishers":
+            print "running " + action_type
+            tiids = update_active_publisher_items(number_to_update, myredis, mydao)
+        elif action_type == "least_recently_updated":
+            print "running " + action_type
             tiids = update_least_recently_updated(number_to_update, myredis, mydao)
     except (KeyboardInterrupt, SystemExit): 
         # this approach is per http://stackoverflow.com/questions/2564137/python-how-to-terminate-a-thread-when-main-program-ends
@@ -195,11 +214,9 @@ def main(run_publisher=False, run_least_recently_updated=True):
 if __name__ == "__main__":
     # get args from the command line:
     parser = argparse.ArgumentParser(description="Run periodic metrics updating from the command line")
-    #parser.add_argument("action_type", type=str, help="The action to test; available actions listed in fakes.py")
+    parser.add_argument("action_type", type=str, help="The action to test; available actions are 'active_publishers' and 'least_recently_updated'")
+    parser.add_argument('--number_to_update', default='35', type=int, help="Number to update.")
     args = vars(parser.parse_args())
     print args
     print "updater.py starting."
-    run_publisher = False
-    run_least_recently_updated = True
-
-    main(run_publisher, run_least_recently_updated)
+    main(args["action_type"], args["number_to_update"])
