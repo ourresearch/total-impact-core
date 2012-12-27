2ec4cc71751ee836f2ddecdb92ccdb39233f9886
Heather Piwowar <hpiwowar@gmail.com>
2012-12-27 00:56:43 -0800

    placeholders for other functions

--- totalimpact/api_user.py
+++ totalimpact/api_user.py
@@ -15,10 +15,31 @@ def make(prefix, **meta):
     api_user_doc["meta"] = meta
     api_user_doc["current_key"] = new_api_key
     api_user_doc["key_history"] = {now: new_api_key}
+    api_user_doc["max_registered_items"] = 1000
     api_user_doc["registered_items"] = {}
 
     return (api_user_doc, new_api_key)
 
+
+def register_item(alias, tiid, api_key):
+    if number_of_remaining_registration_spots(api_key) <= 0:
+        #throw error
+        pass
+
+    # do the registering
+    now = datetime.datetime.now().isoformat()
+
+    api_user_doc["registered_items"][alias] = {
+        "registered_date" = now,
+        "tiid" = tiid
+    }
+    return(number_of_remaining_registration_spots)
+
+
+def number_of_remaining_registration_spots(api_key):
+    number_of_items_registered = len(api_user_doc["registered_items"])    
+    number_of_items_remaining = api_user_doc["max_registered_items"] - number_of_items_registered
+    return(number_of_items_remaining)
 
 
 
