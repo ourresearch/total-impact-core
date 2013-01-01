37149f14dd2481d9cc7b1129062643bdf6a2f8d1
Heather Piwowar <hpiwowar@gmail.com>
2013-01-01 08:04:22 -0800

    add PLOS full-text search provider

---
+++ test/unit_tests/providers/test_plossearch.py
@@ -0,0 +1,60 @@
+from test.unit_tests.providers import common
+from test.unit_tests.providers.common import ProviderTestCase
+from totalimpact.providers.provider import Provider, ProviderContentMalformedError
+from test.utils import http
+
+import os
+import collections
+from nose.tools import assert_equals, raises, nottest
+
+datadir = os.path.join(os.path.split(__file__)[0], "../../../extras/sample_provider_pages/plossearch")
+SAMPLE_EXTRACT_METRICS_PAGE = os.path.join(datadir, "metrics")
+
+class TestPlossearch(ProviderTestCase):
+
+    provider_name = "plossearch"
+
+    testitem_aliases = ("url", "http://hdl.handle.net/10255/dryad.235")
+    testitem_metrics = ("url", "http://hdl.handle.net/10255/dryad.235")
+
+    def setUp(self):
+        ProviderTestCase.setUp(self)
+
+    def test_is_relevant_alias(self):
+        # ensure that it matches an appropriate ids
+        assert_equals(self.provider.is_relevant_alias(self.testitem_aliases), True)
+
+    def test_extract_metrics_success(self):
+        f = open(SAMPLE_EXTRACT_METRICS_PAGE, "r")
+        good_page = f.read()
+        metrics_dict = self.provider._extract_metrics(good_page)
+        expected = {'plossearch:mentions': 1}
+        assert_equals(metrics_dict, expected)
+
+    def test_provenance_url(self):
+        provenance_url = self.provider.provenance_url("tweets", 
+            [self.testitem_aliases])
+        expected = 'http://www.plosone.org/search/advanced?queryTerm=&unformattedQuery=everything:"http%3A//hdl.handle.net/10255/dryad.235"'
+        assert_equals(provenance_url, expected)
+
+    @http
+    def test_metrics(self):
+        metrics_dict = self.provider.metrics([self.testitem_metrics])
+        expected = {'plossearch:mentions': (2, 'http://www.plosone.org/search/advanced?queryTerm=&unformattedQuery=everything:"http%3A//hdl.handle.net/10255/dryad.235"')}
+        print metrics_dict
+        for key in expected:
+            assert metrics_dict[key][0] >= expected[key][0], [key, metrics_dict[key], expected[key]]
+            assert metrics_dict[key][1] == expected[key][1], [key, metrics_dict[key], expected[key]]
+
+    @http
+    def test_metrics_multiple_urls(self):
+        metrics_dict = self.provider.metrics([("url","http://dx.doi.org/10.5061/dryad.234"), 
+                                                ("doi", "10.5061/dryad.234"), 
+                                                ("url","http://hdl.handle.net/10255/dryad.235")])
+        expected = {'plossearch:mentions': (2, 'http://www.plosone.org/search/advanced?queryTerm=&unformattedQuery=everything:"http%3A//hdl.handle.net/10255/dryad.235"')}
+        print metrics_dict
+        for key in expected:
+            assert metrics_dict[key][0] >= expected[key][0], [key, metrics_dict[key], expected[key]]
+            assert metrics_dict[key][1] == expected[key][1], [key, metrics_dict[key], expected[key]]
+
+
---
+++ totalimpact/providers/plossearch.py
@@ -0,0 +1,60 @@
+from totalimpact.providers import provider
+from totalimpact.providers.provider import Provider, ProviderContentMalformedError
+
+import simplejson, re, os
+
+import logging
+logger = logging.getLogger('ti.providers.plossearch')
+
+class Plossearch(Provider):  
+
+    example_id = ("doi", "10.5061/dryad.c2b53")
+
+    url = "http://www.plos.org/"
+    descr = "PLoS article level metrics."
+    metrics_url_template = 'http://api.plos.org/search?q="%s"&api_key=' + os.environ["PLOS_KEY"]
+    provenance_url_template = 'http://www.plosone.org/search/advanced?queryTerm=&unformattedQuery=everything:"%s"'
+
+    static_meta_dict =  {
+        "mentions": {
+            "display_name": "mentions",
+            "provider": "PLoS",
+            "provider_url": "http://www.plos.org/",
+            "description": "the number of times the doi was mentioned in the full-text of PLOS papers",
+            "icon": "http://a0.twimg.com/profile_images/67542107/Globe_normal.jpg" ,
+        }
+    } 
+
+    def __init__(self):
+        super(Plossearch, self).__init__()
+
+    def is_relevant_alias(self, alias):
+        (namespace, nid) = alias
+        if namespace in ["url", "doi"]:
+            for host in ["dryad", "figshare", "github"]:
+                if host in nid:
+                    return True
+        return False
+
+    def get_best_id(self, aliases):
+        return self.get_relevant_alias_with_most_metrics("plossearch:mentions", aliases)
+
+    def _extract_metrics(self, page, status_code=200, id=None):
+        if status_code != 200:
+            if status_code == 404:
+                return {}
+            else:
+                raise(self._get_error(status_code))
+
+        if not "<response>" in page:
+            raise ProviderContentMalformedError
+
+        count = provider._count_in_xml(page, 'doc')
+        if count:
+            metrics_dict = {'plossearch:mentions': count}
+        else:
+            metrics_dict = {}
+
+        return metrics_dict
+
+
