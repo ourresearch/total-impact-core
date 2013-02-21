ba3cb0972f4dbcc698e2c9f468f49cfd9e560b7d
Heather Piwowar <hpiwowar@gmail.com>
2013-02-21 11:25:13 -0800

    store email received at /inbox endpoint
    code should probably be moved to its own file at some point, but not sure yet where

--- test/unit_tests/test_views.py
+++ test/unit_tests/test_views.py
@@ -622,12 +622,50 @@ class TestApi(ViewsTester):
         assert_equals(first_plos_create_tiid, second_plos_create_tiid)
 
     def test_inbox(self):
-        resp = self.client.post(
+        # example from http://docs.cloudmailin.com/http_post_formats/json/
+        example_payload = {
+              "headers": {
+                "Return-Path": "from@example.com",
+                "Received": [
+                  "by 10.52.90.229 with SMTP id bz5cs75582vdb; Mon, 16 Jan 2012 09:00:07 -0800",
+                  "by 10.216.131.153 with SMTP id m25mr5479776wei.9.1326733205283; Mon, 16 Jan 2012 09:00:05 -0800",
+                  "from mail-wi0-f170.google.com (mail-wi0-f170.google.com [209.85.212.170]) by mx.google.com with ESMTPS id u74si9614172weq.62.2012.01.16.09.00.04 (version=TLSv1/SSLv3 cipher=OTHER); Mon, 16 Jan 2012 09:00:04 -0800"
+                ],
+                "Date": "Mon, 16 Jan 2012 17:00:01 +0000",
+                "From": "Message Sender <sender@example.com>",
+                "To": "Message Recipient<to@example.co.uk>",
+                "Message-ID": "<4F145791.8040802@example.com>",
+                "Subject": "Test Subject",
+                "Mime-Version": "1.0",
+                "Content-Type": "multipart/alternative; boundary=------------090409040602000601080801",
+                "Delivered-To": "to@example.com",
+                "Received-SPF": "neutral (google.com: 10.0.10.1 is neither permitted nor denied by best guess record for domain of from@example.com) client-ip=10.0.10.1;",
+                "Authentication-Results": "mx.google.com; spf=neutral (google.com: 10.0.10.1 is neither permitted nor denied by best guess record for domain of from@example.com) smtp.mail=from@example.com",
+                "User-Agent": "Postbox 3.0.2 (Macintosh/20111203)"
+              },
+              "envelope": {
+                "to": "to@example.com",
+                "from": "from@example.com",
+                "helo_domain": "localhost",
+                "remote_ip": "127.0.0.1",
+              },
+              "plain": "Test with HTML.",
+              "html": "<html><head>\n<meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\"></head><body\n bgcolor=\"#FFFFFF\" text=\"#000000\">\nTest with <span style=\"font-weight: bold;\">HTML</span>.<br>\n</body>\n</html>",
+              "reply_plain": "Message reply if found.",
+              "attachments": []
+            }
+
+        response = self.client.post(
             "/v1/inbox?key=validkey",
-            data=json.dumps({}),
+            data=json.dumps(example_payload),
             content_type="application/json"
         )
-        assert_equals(200, resp.status_code)
+        assert_equals(200, response.status_code)
+
+        doc_id = json.loads(response.data)["_id"]
+        stored_doc = mydao.get(doc_id)
+        assert_equals(stored_doc.keys(), ['_rev', '_id', 'type', 'payload', 'created'])
+        assert_equals(stored_doc["payload"], example_payload)
 
 
 class TestTiid(ViewsTester):
