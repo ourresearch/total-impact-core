e23fe338445eb4e0d5719b5fb9b3f85fa4857306
Heather Piwowar <hpiwowar@gmail.com>
2013-08-27 14:09:12 -0700

    add a few comments to make ti clear what create_sqlalchemy_tables.py script does

--- extras/db_housekeeping/create_sqlalchemy_tables.py
+++ extras/db_housekeeping/create_sqlalchemy_tables.py
@@ -1,3 +1,9 @@
+# creates all sqlalchemy tables
+# will create tables that don't exist yet and leave untouched those already there
+# heroku run python extras/db_housekeeping/create_sqlalchemy_tables.py --app total-impact-core-staging
+
+# need to start with the import because that also imports the sqlalchemy class definitions
+
 from totalimpact import db
 db.create_all()
 
