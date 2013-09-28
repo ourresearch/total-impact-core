d467c84ae31b320d3245edc606a0f4e7957b3cdf
Heather Piwowar <hpiwowar@gmail.com>
2013-09-28 06:46:22 -0700

    don't remove spaces as nonprinting characters

--- totalimpact/unicode_helpers.py
+++ totalimpact/unicode_helpers.py
@@ -11,6 +11,16 @@ def to_unicode_or_bust(obj, encoding='utf-8'):
     return obj
 
 
+def printing_character_or_space(c):
+    is_printing_character_or_space = True
+    # see http://www.fileformat.info/info/unicode/category/index.htm
+    char_classes_to_remove = ["C", "M", "Z"]
+    if unicodedata.category(c)[0] in char_classes_to_remove:
+        if c != " ":
+            is_printing_character_or_space = False
+    return is_printing_character_or_space
+
+
 def remove_nonprinting_characters(input, encoding='utf-8'):
     input_was_unicode = True
     if isinstance(input, basestring):
@@ -19,10 +29,8 @@ def remove_nonprinting_characters(input, encoding='utf-8'):
 
     unicode_input = to_unicode_or_bust(input)
 
-    # see http://www.fileformat.info/info/unicode/category/index.htm
-    char_classes_to_remove = ["C", "M", "Z"]
 
-    response = u''.join(c for c in unicode_input if unicodedata.category(c)[0] not in char_classes_to_remove)
+    response = u''.join(c for c in unicode_input if printing_character_or_space(c))
 
     if not input_was_unicode:
         response = response.encode(encoding)
