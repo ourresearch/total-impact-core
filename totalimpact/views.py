d4f1e31dfbf3d759ad6259b22fdbcc119cf7bcf9
Jason Priem <jp@jasonpriem.org>
2013-10-02 23:02:15 -0400

    Importer endpoint returns a dict of tiids instead of a list.
    Turned out Angular liked it better this way, if it's coming back from a post; I was wrong the first time.

--- totalimpact/views.py
+++ totalimpact/views.py
@@ -341,10 +341,10 @@ def importer_post(provider_name):
     logger.debug(u"in provider_importer_get with {tiids_aliases_map}".format(
         tiids_aliases_map=tiids_aliases_map)) 
 
-    tiids_response_list = [{"tiid": tiid} for tiid in tiids_aliases_map.keys()]
+    products_dict = {k: {} for k in tiids_aliases_map.keys()}
 
     resp = make_response(
-        json.dumps(tiids_response_list, sort_keys=True, indent=4),
+        json.dumps({"products": products_dict}, sort_keys=True, indent=4),
         200
     )
     return resp
