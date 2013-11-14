cfa008c63e6fe5877fb6dcb5a4f20bd5d918632f
Heather Piwowar <hpiwowar@gmail.com>
2013-11-14 17:44:18 -0500

    fix incomplete unittest

--- test/unit_tests/providers/test_bibjson.py
+++ test/unit_tests/providers/test_bibjson.py
@@ -58,6 +58,36 @@ class TestBibjson(ProviderTestCase):
 
         print json.dumps(response, indent=4)
 
-        expected = "hi"
+        expected = [
+              (
+                  "biblio", 
+                  {
+                      "rawString": "Piwowar, H.A. and Chapman, W.W., (2008). Identifying data sharing in biomedical literature., AMIA Annual Symposium proceedings / AMIA Symposium. AMIA Symposium, pp. 596-600", 
+                      "title": "AMIA Annual Symposium proceedings / AMIA Symposium. AMIA Symposium,", 
+                      "first_author": "Piwowar", 
+                      "booktitle": "AMIA Annual Symposium proceedings / AMIA Symposium. AMIA Symposium,", 
+                      "year": "2008", 
+                      "first_page": "596", 
+                      "authors": "Piwowar, Chapman", 
+                      "marker": "Piwowar, Chapman, 2008", 
+                      "pages": "596--600"
+                  }
+              ), 
+              (
+                  "biblio", 
+                  {
+                      "volume": "4", 
+                      "first_author": "Piwowar", 
+                      "first_page": "148", 
+                      "rawString": "Piwowar, H.A. and Chapman, W.W., (2010). Public sharing of research datasets: A pilot study of associations, Journal of Informetrics, vol. 4, no. 2, pp. 148-156", 
+                      "authors": "Piwowar, Chapman", 
+                      "marker": "Piwowar, Chapman, 2010", 
+                      "journal": "Journal of Informetrics,", 
+                      "title": "Public sharing of research datasets: A pilot study of associations,", 
+                      "year": "2010", 
+                      "pages": "148--156"
+                  }
+              )
+          ]
         assert_equals(response, expected)
 
