905bea651ad505d261858446876089a10d69424a
Heather Piwowar <hpiwowar@gmail.com>
2014-05-20 11:28:59 -0700

    try again

--- celeryconfig.py
+++ celeryconfig.py
@@ -9,16 +9,20 @@ sys.path.append('.')
 # CELERY_RESULT_BACKEND = "amqp"
 
 
-redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/')  # include trailing /
+# redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/')  # include trailing /
 # redis_url_parsed = urlparse.urlparse(redis_url)
+
+redis_url = os.environ.get('REDIS_URL')
+if not redis_url.endswith("/"):
+    redis_url += "/"
 
 BROKER_URL = redis_url + "0"
 # REDIS_HOST = redis_url_parsed.hostname
 # REDIS_PORT = redis_url_parsed.port
 # REDIS_PASSWORD = redis_url_parsed.password
 # REDIS_DB = 0
-REDIS_CONNECT_RETRY = True
 CELERY_RESULT_BACKEND = redis_url + "0"
+REDIS_CONNECT_RETRY = True
 
 # these options will be defaults in future as per http://celery.readthedocs.org/en/latest/getting-started/brokers/redis.html
 BROKER_TRANSPORT_OPTIONS = {'fanout_prefix': True}
