6b0b65c8b18fcb8b3465f215001585b57adff6c2
Heather Piwowar <hpiwowar@gmail.com>
2014-05-26 13:53:32 -0700

    support scaling to more dynos by including dyno name in celery worker name

--- celeryworkers.sh
+++ celeryworkers.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
+
 for ((i=1; i<=$CELERY_MULTI_WORKERS; i++))
 do
-  COMMAND="celery worker -n core-celeryworker:$DYNO:${i} --loglevel=info --config=celeryconfig --events --concurrency=$CELERY_CONCURRENCY"
+  COMMAND="celery worker -n core-$DYNO:${i} --loglevel=info --config=celeryconfig --events --concurrency=$CELERY_CONCURRENCY"
   echo $COMMAND
   $COMMAND&
 done
