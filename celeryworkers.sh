4ea41d0cd56d0053d04b3207abe6162d34d04507
Heather Piwowar <hpiwowar@gmail.com>
2014-05-26 13:48:21 -0700

    allow to scale to more dynos by including dyno number in worker name

--- celeryworkers.sh
+++ celeryworkers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for ((i=1; i<=$CELERY_MULTI_WORKERS; i++))
 do
-  COMMAND="celery worker -n core-celeryworker${i} --loglevel=info --config=celeryconfig --events --concurrency=$CELERY_CONCURRENCY"
+  COMMAND="celery worker -n core-celeryworker:$DYNO:${i} --loglevel=info --config=celeryconfig --events --concurrency=$CELERY_CONCURRENCY"
   echo $COMMAND
   $COMMAND&
 done
