d4aa221f8224183b8f1688ff0cb6f10359459e5e
Heather Piwowar <hpiwowar@gmail.com>
2014-05-27 14:54:06 -0700

    increase logging from celery

--- celeryworkers.sh
+++ celeryworkers.sh
@@ -3,7 +3,7 @@
 
 for ((i=1; i<=$CELERY_MULTI_WORKERS; i++))
 do
-  COMMAND="celery worker -n core-$DYNO:${i} --loglevel=warning --config=celeryconfig --events --concurrency=$CELERY_CONCURRENCY"
+  COMMAND="celery worker -n core-$DYNO:${i} --loglevel=info --config=celeryconfig --events --concurrency=$CELERY_CONCURRENCY"
   echo $COMMAND
   $COMMAND&
 done
